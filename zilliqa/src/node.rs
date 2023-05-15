211caa6f27f8ac90fa1c7b2331640d8c824fdbbc
James Hinshelwood <jameshinshelwood1@gmail.com>
2023-05-15 18:22:57 +0100

    Fix `eth_getBlockByNumber` with `"latest"` parameter
    `Consensus::view()` tells us the view of the _next_ block, so to
    return the latest block, we need to return the block at
    `Consensus::view() - 1`.

--- zilliqa/src/node.rs
+++ zilliqa/src/node.rs
@@ -125,7 +125,7 @@ impl Node {
     }
 
     pub fn get_latest_block(&self) -> Option<&Block> {
-        self.get_block_by_view(self.consensus.view())
+        self.get_block_by_view(self.consensus.view() - 1)
     }
 
     pub fn get_block_by_view(&self, view: u64) -> Option<&Block> {
