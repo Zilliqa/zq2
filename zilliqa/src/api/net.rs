0dd7f7e150d83020b4d913ef414fc82eb44d3052
James Hinshelwood <JamesHinshelwood@users.noreply.github.com>
2023-05-30 12:47:28 +0100

    Refactor RPC module declarations (#187)
    We move the `method!` macro from `eth.rs` into `api/mod.rs` so it
    can be used by other APIs as well. Also, instead of exposing a
    `method!` macro which expects various variables to be in scope and
    is called multiple times, we instead expose a `declare_module!`
    macro which should be called once with a list of methods and
    returns a full `RpcModule`.
    
    This also moves the single Zilliqa API method that we've
    implemented to the same macro.

---
+++ zilliqa/src/api/net.rs
@@ -0,0 +1,14 @@
+use std::sync::{Arc, Mutex};
+
+use anyhow::Result;
+use jsonrpsee::{types::Params, RpcModule};
+
+use crate::node::Node;
+
+pub fn rpc_module(node: Arc<Mutex<Node>>) -> RpcModule<Arc<Mutex<Node>>> {
+    super::declare_module!(node, [("net_version", version)])
+}
+
+fn version(_: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
+    Ok(node.lock().unwrap().config.eth_chain_id.to_string())
+}
