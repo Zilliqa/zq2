406131a305d73a4e13d0a797ce68800135ef8265
n-hutton <nathan.cameron.hutton@gmail.com>
2023-06-02 21:58:24 +0100

    thing

--- scripts/js_test.sh
+++ scripts/js_test.sh
@@ -5,8 +5,8 @@ cd evm_js_tests
 # Install dependencies silently on the CI server
 
 # install dependencies
-apt update
-apt -y install gpg python3 lsb-core curl dirmngr apt-transport-https lsb-release ca-certificates
+sudo apt update
+sudo apt -y install gpg python3 lsb-core curl dirmngr apt-transport-https lsb-release ca-certificates
 ## Adding the NodeSource signing key to your keyring...
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee /usr/share/keyrings/nodesource.gpg >/dev/null
 
@@ -15,12 +15,16 @@ curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | t
 echo 'deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x jammy main' > /etc/apt/sources.list.d/nodesource.list
 echo 'deb-src [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x jammy main' >> /etc/apt/sources.list.d/nodesource.list
 
-apt update
-apt -y install nodejs
+sudo apt update
+sudo apt -y install nodejs
 node --version
+
+cd -
 
 find ./ -name zilliqa
 echo "should be here, right"
 ls ./target/debug/zilliqa
+
+cd -
 
 npx hardhat test --network zq2
