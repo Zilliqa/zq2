5248eee634e42f1eb9fdd26a977d09fda7a95072
n-hutton <nathan.cameron.hutton@gmail.com>
2023-06-04 19:23:56 +0100

    .

--- scripts/js_test.sh
+++ scripts/js_test.sh
@@ -34,6 +34,46 @@ ls
 
 npm install
 echo $PATH
-sudo touch /bin/scilla-fmt
-sudo chmod 755 /bin/scilla-fmt
+
+#sudo apt-get update \
+#    && apt-get install -y software-properties-common \
+#    && add-apt-repository ppa:avsm/ppa -y \
+#    && apt-get update && apt-get install -y --no-install-recommends \
+#    git \
+#    curl \
+#    wget \
+#    cmake \
+#    build-essential \
+#    m4 \
+#    ocaml \
+#    opam \
+#    pkg-config \
+#    zlib1g-dev \
+#    libgmp-dev \
+#    libffi-dev \
+#    libssl-dev \
+#    libsecp256k1-dev \
+#    libboost-system-dev \
+#    libboost-test-dev \
+#    libboost-dev \
+#    libpcre3-dev \
+#    && rm -rf /var/lib/apt/lists/*
+#
+#export OCAML_VERSION=4.11.2
+#
+## CMake gets installed here
+#export PATH="/root/.local/bin:${PATH}"
+#
+#wget https://github.com/Kitware/CMake/releases/download/v3.19.3/cmake-3.19.3-Linux-x86_64.sh
+#mkdir -p "${HOME}"/.local
+#bash ./cmake-3.19.3-Linux-x86_64.sh --skip-license --prefix="${HOME}"/.local/
+#
+#RUN bash scripts/install_cmake_ubuntu.sh \
+#    && make opamdep-ci \
+#    && echo '. ~/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true ' >> ~/.bashrc \
+#    && eval $(opam env) && \
+#    make
+#
+##sudo touch /bin/scilla-fmt
+##sudo chmod 755 /bin/scilla-fmt
 npx hardhat test --network zq2
