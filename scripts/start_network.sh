3dbec57756553478e3f6ced208526cd3574cc0fb
n-hutton <nathan.cameron.hutton@gmail.com>
2023-06-09 01:26:49 +0100

    .

--- scripts/start_network.sh
+++ scripts/start_network.sh
@@ -43,6 +43,8 @@ source ~/.profile
 
 echo "now switch... "
 export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install 16.0
 nvm use 16.0
