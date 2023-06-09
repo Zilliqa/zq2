7ca95fa92ba149c919ea946cfb21ae10f741a279
n-hutton <nathan.cameron.hutton@gmail.com>
2023-06-09 01:57:29 +0100

    .

--- scripts/js_test.sh
+++ scripts/js_test.sh
@@ -2,8 +2,8 @@ cd evm_js_tests
 echo $PATH
 echo "now we run the test..."
 
-npm install
-DEBUG=true MOCHA_TIMEOUT=400000 npx hardhat test
+#npm install
+DEBUG=true MOCHA_TIMEOUT=400000 npx hardhat test --grep "should return a send raw transaction" --bail
 
 retVal=$?
 
