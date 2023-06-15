6f51916363b6dda2e8d886e415085092f6b31984
James Hinshelwood <JamesHinshelwood@users.noreply.github.com>
2023-06-15 08:41:58 +0100

    Implement `eth_getStorageAt` (#228)
    * Implement `eth_getStorageAt`
    
    In order to test this with the provided example, I added a helper
    macro to the test suite to deploy contracts, given their source. I
    decided it would be simplest to just compile them at runtime and I
    figured we could revisit this decision quite easily later if that
    turns out to be slow.

---
+++ zilliqa/tests/it/contracts/Storage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+contract Storage {
+    uint pos0;
+    mapping(address => uint) pos1;
+    constructor() {
+        pos0 = 1234;
+        pos1[msg.sender] = 5678;
+    }
+}
