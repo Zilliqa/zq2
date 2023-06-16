e40dd9aea7b6b4a25f10ad465fae0632d3ce82ee
Theodore Bugnet <theodore@zilliqa.com>
2023-06-16 17:06:43 +0100

    WIP

--- zilliqa/src/bin/zilliqa.rs
+++ zilliqa/src/bin/zilliqa.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use anyhow::Result;
 use clap::{CommandFactory, Parser};
@@ -9,23 +9,25 @@ use libp2p::{
     mdns,
     swarm::NetworkBehaviour,
 };
+use merge::Merge;
 use opentelemetry::runtime;
 use opentelemetry_otlp::{ExportConfig, WithExportConfig};
 use tokio::time::Duration;
-use twelf::{config, Layer};
 
-use zilliqa::{cfg::Config, crypto::SecretKey, node_launcher::NodeLauncher};
+use zilliqa::{
+    cfg::{Config, ConfigOpt},
+    crypto::SecretKey,
+    node_launcher::NodeLauncher,
+};
 
-#[config]
 #[derive(Parser, Debug)]
 struct Args {
     #[clap(value_parser = SecretKey::from_hex)]
     secret_key: SecretKey,
     #[clap(long, short, default_value = "config.toml")]
     config_file: PathBuf,
-    #[serde(flatten)]
     #[clap(flatten)]
-    config: Config,
+    config: ConfigOpt,
 }
 
 #[derive(NetworkBehaviour)]
@@ -40,33 +42,23 @@ struct Behaviour {
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    // let args = Args::parse();
-    let args = Args::command().get_matches();
-    // TODO: override config path through env variable?
-    let config_path = args.get_one::<PathBuf>("config_file");
-    println!("Args parsed; config path: {config_path:?}");
+    let args = Args::parse();
+    println!("Args parsed; config path: {:?}", args.config_file);
 
-    let mut layers: Vec<Layer> = vec![];
-    layers.push(Layer::Clap(args.clone()));
-    if let Some(path) = config_path {
-        if path.exists() {
-            layers.push(Layer::Toml(path.clone()));
-            println!("Pushing config file layer");
-        } else {
-            println!("Skipping toml layer...");
-        }
-    }
-    std::env::set_var("ZQ2_ETH_CHAIN_ID", "4000");
-    layers.push(Layer::Env(Some("ZQ2_".to_string())));
-    // layers.push(Layer::Env(None));
-
-    println!("Layers set up");
-    let args = Args::with_layers(&layers)?;
+    let toml_config = if args.config_file.exists() {
+        println!("Parsing toml file");
+        toml::from_str::<ConfigOpt>(&fs::read_to_string(&args.config_file)?)?
+    } else {
+        println!("Skipping toml file");
+        ConfigOpt::default()
+    };
 
     println!("{args:?}");
-    return Ok(());
+    let mut config = args.config;
+    config.merge(toml_config);
 
-    let config = args.config;
+    println!("{config:?}");
+    return Ok(());
 
     let p2p_port = config.p2p_port;
     if let Some(endpoint) = &config.otlp_collector_endpoint {
--- zilliqa/src/cfg.rs
+++ zilliqa/src/cfg.rs
@@ -1,25 +1,38 @@
 use clap::Args;
-use serde::{Deserialize, Serialize};
+use merge::Merge;
+use serde::Deserialize;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Args)]
+/// Helper struct used for deserializing the config file and parsing command line arguments.
+/// Fields must match Config struct, but every field should be optional.
+#[derive(Debug, Clone, Default, Deserialize, Args, Merge)]
 #[serde(default, deny_unknown_fields)]
-pub struct Config {
-    /// The port to listen for P2P messages on. 0 means random.
-    #[clap(long, required = false)]
-    pub p2p_port: u16,
-    /// The port to listen for JSON-RPC requests on. Defaults to 4201.
-    #[clap(long, default_value = "4201")]
-    pub json_rpc_port: u16,
-    /// Pass this to disable listening to JSON-RPC requests entirely.
-    #[clap(long, default_value = "false")]
-    pub disable_json_rpc: bool,
-    #[clap(long, required = false)] // 32768 + 1 = 0x8000 + 1
-    pub eth_chain_id: u64,
-    /// The base address of the OTLP collector. If not set, metrics will not be exported.
+pub struct ConfigOpt {
+    #[clap(long)]
+    pub p2p_port: Option<u16>,
+    #[clap(long)]
+    pub json_rpc_port: Option<u16>,
+    #[clap(long)]
+    pub disable_json_rpc: Option<bool>,
+    #[clap(long)]
+    pub eth_chain_id: Option<u64>,
     #[clap(long)]
     pub otlp_collector_endpoint: Option<String>,
+    #[clap(long)]
+    pub allowed_timestamp_skew: Option<u64>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    /// The port to listen for P2P messages on. 0 means random.
+    pub p2p_port: u16,
+    /// The port to listen for JSON-RPC requests on. Defaults to 4201.
+    pub json_rpc_port: u16,
+    /// Pass this to disable listening to JSON-RPC requests entirely.
+    pub disable_json_rpc: bool,
+    pub eth_chain_id: u64,
+    /// The base address of the OTLP collector. If not set, metrics will not be exported.
+    pub otlp_collector_endpoint: Option<String>,
     /// The maximum duration between a recieved block's timestamp and the current time, in seconds.
-    #[clap(long, default_value = "10")]
     pub allowed_timestamp_skew: u64,
 }
 
