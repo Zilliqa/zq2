46debd4bbb83e5652b0b908c626bdfd5874c58da
n-hutton <nathan.cameron.hutton@gmail.com>
2023-07-21 15:48:24 +0100

    Update zilliqa/src/exec.rs
    Co-authored-by: James Hinshelwood <JamesHinshelwood@users.noreply.github.com>

--- zilliqa/src/exec.rs
+++ zilliqa/src/exec.rs
@@ -225,7 +225,7 @@ impl State {
                 acct.nonce = acct.nonce.checked_add(1).unwrap();
                 self.save_account(txn.from_addr, acct)?;
 
-                info!("Finished executing TX: {}", hash);
+                info!(?hash, "finished executing txn");
                 Ok(TransactionApplyResult {
                     success,
                     contract_address: contract_addr.map(Address),
