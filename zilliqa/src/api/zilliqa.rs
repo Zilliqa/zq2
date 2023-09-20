646b9510cee0373adc6e5245ffdf731b5392a8ae
James Hinshelwood <jameshinshelwood1@gmail.com>
2023-09-20 12:18:08 +0100

    Implement Zilliqa `GetBalance` API

--- zilliqa/src/api/zilliqa.rs
+++ zilliqa/src/api/zilliqa.rs
@@ -4,17 +4,35 @@ use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 use jsonrpsee::{types::Params, RpcModule};
+use primitive_types::H160;
+use serde_json::json;
 
-use crate::node::Node;
+use crate::{message::BlockNumber, node::Node, state::Address};
 
 pub fn rpc_module(node: Arc<Mutex<Node>>) -> RpcModule<Arc<Mutex<Node>>> {
     super::declare_module!(
         node,
         [
+            ("GetBalance", get_balance),
             ("GetCurrentMiniEpoch", get_current_mini_epoch),
             ("GetVersion", get_git_commit),
         ],
     )
+}
+
+fn get_balance(params: Params, node: &Arc<Mutex<Node>>) -> Result<serde_json::Value> {
+    let address: H160 = params.one()?;
+
+    let node = node.lock().unwrap();
+
+    let balance = node
+        .get_native_balance(Address(address), BlockNumber::Latest)?
+        .to_string();
+    let nonce = node
+        .get_account(Address(address), BlockNumber::Latest)?
+        .nonce;
+
+    Ok(json!({"balance": balance, "nonce": nonce}))
 }
 
 fn get_current_mini_epoch(_: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
