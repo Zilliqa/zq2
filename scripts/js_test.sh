162e9611bda0863617f0624f456203b751af0738
n-hutton <nathan.cameron.hutton@gmail.com>
2023-10-29 17:54:17 +0000

    Update js_test.sh

--- scripts/js_test.sh
+++ scripts/js_test.sh
@@ -3,9 +3,7 @@ echo "The CI is running this script."
 # Start network early.
 pwd
 cargo build --all-targets > /dev/null 2>&1
-#RUST_LOG=zilliqa=trace ./target/debug/z2 internal run > /tmp/zil_log_out.txt 2>&1 &
 RUST_LOG=zilliqa=warn ./target/debug/zilliqa 65d7f4da9bedc8fb79cbf6722342960bbdfb9759bc0d9e3fb4989e831ccbc227 -c config-example.toml > /tmp/zil_log_out.txt 2>&1 &
-#sleep 10;
 
 # Pull submodule
 cd evm_js_tests
@@ -50,7 +48,7 @@ retVal=$?
 
 pkill -INT zilliqa
 if [ $retVal -ne 0 ]; then
-    cat /tmp/zil_log_out.txt
+    cat /tmp/zil_log_out.txt | /usr/local/bin/ansi2txt
     echo "!!!!!! Error with JS integration test !!!!!!"
     exit 1
 fi
