29976812b0ac2f878b9e3b05741a392f1f2344c2
n-hutton <nathan.cameron.hutton@gmail.com>
2023-11-20 23:26:13 +0000

    .

--- zilliqa/tests/it/consensus.rs
+++ zilliqa/tests/it/consensus.rs
@@ -113,7 +113,7 @@ async fn block_production(mut network: Network) {
                     .map_or(0, |b| b.number())
                     >= 10
             },
-            5000,
+            500000,
         )
         .await
         .unwrap();
