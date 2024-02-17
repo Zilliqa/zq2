d1ebd1e950ac5b63c06d8941067efd8b1314db78
n-hutton <nathan.cameron.hutton@gmail.com>
2024-02-17 23:23:22 +0000

    this

--- evm_scilla_js_tests/test/rpc/eth_gasPrice.ts
+++ evm_scilla_js_tests/test/rpc/eth_gasPrice.ts
@@ -15,7 +15,7 @@ describe(`Calling ${METHOD} #parallel`, function () {
       assert.match(result.result, /^0x/, "should be HEX starting with 0x");
       assert.isNumber(+result.result, "can be converted to a number");
 
-      const expectedMinGasPrice = 1000000; // default minimum Zilliqa gas price in wei
+      const expectedMinGasPrice = 1; // default minimum Zilliqa gas price in wei
       assert.isAtLeast(+result.result, expectedMinGasPrice, "should have a gas price " + expectedMinGasPrice);
     });
   });
