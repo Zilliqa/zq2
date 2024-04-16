e838b695a143b0d567a9386a363493bcc4a9c4e1
Bartosz Zawistowski <39065214+bzawisto@users.noreply.github.com>
2024-04-16 09:13:01 +0200

    Adding ERC20 precompile (#804)
    * Adding ERC20 precompile stub
    
    * Build additional precompile set in separate method
    
    * Some progress with precompiles
    
    * Some progress with erc20 precompile
    
    * Cleanup
    
    * Cleanup (2)
    
    * Get back to the previous chain Id
    
    * Naming
    
    * Cargo.lock file
    
    * Adding ERC20 precompile stub
    
    * Build additional precompile set in separate method
    
    * Some progress with precompiles
    
    * Some progress with erc20 precompile
    
    * Cleanup
    
    * Cleanup (2)
    
    * Get back to the previous chain Id
    
    * Naming
    
    * Cargo.lock file
    
    * Fmt
    
    * Remove yarn file
    
    * Review feedback
    
    ---------
    
    Co-authored-by: Bartosz Zawistowski <bartosz@zilliqa.com>

--- zilliqa/src/exec.rs
+++ zilliqa/src/exec.rs
@@ -4,6 +4,7 @@ use std::{
     error::Error,
     fmt::{self, Display, Formatter},
     num::NonZeroU128,
+    sync::Arc,
 };
 
 use anyhow::{anyhow, Result};
@@ -24,6 +25,7 @@ use crate::{
     crypto::{Hash, NodePublicKey},
     eth_helpers::extract_revert_msg,
     message::BlockHeader,
+    precompiles::get_custom_precompiles,
     state::{contract_addr, Account, Address, State},
     time::SystemTime,
     transaction::{Log, VerifiedTransaction},
@@ -220,6 +222,14 @@ impl State {
                 gas_priority_fee: None,
                 blob_hashes: vec![],
                 max_fee_per_blob_gas: None,
+            })
+            .append_handler_register(|handler| {
+                let precompiles = handler.pre_execution.load_precompiles();
+                handler.pre_execution.load_precompiles = Arc::new(move || {
+                    let mut precompiles = precompiles.clone();
+                    precompiles.extend(get_custom_precompiles());
+                    precompiles
+                });
             })
             .build();
 
