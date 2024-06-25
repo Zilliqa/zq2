Zilliqa/zq2

commit 0f34f33e540300ed7dc9ef59efd8f1bbc989ea26

Implement EIP-1898 (#1079)
We now support a `BlockId` for the following APIs:
* `eth_call`
* `eth_getBalance`
* `eth_getCode`
* `eth_getStorageAt`
* `eth_getTransactionCount`

I couldn't resist making a few related changes at the same time:
* We've deleted our `BlockNumber` and now use alloy's `BlockNumberOrTag` instead.
* `Node` is now much pickier about how blocks and state are accessed. Previously, there were many overlapping ways of getting the same blocks or state. Now, everything goes through `get_block` or `get_state`. A load of unused methods have also been removed thanks to this change.

zilliqa/src/api/ots.rs
@@ -3,6 +3,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use alloy_eips::{BlockId, BlockNumberOrTag};
 use alloy_primitives::{Address, B256};
 use anyhow::{anyhow, Result};
 use ethabi::Token;
@@ -17,7 +18,6 @@ use crate::{
     api::to_hex::ToHex,
     crypto::Hash,
     inspector::{self, CreatorInspector, OtterscanOperationInspector, OtterscanTraceInspector},
-    message::BlockNumber,
     node::Node,
     time::SystemTime,
 };
@@ -51,9 +51,9 @@ pub fn get_otterscan_api_level(_: Params, _: &Arc<Mutex<Node>>) -> Result<u64> {
 }
 
 fn get_block_details(params: Params, node: &Arc<Mutex<Node>>) -> Result<Option<ots::BlockDetails>> {
-    let block: u64 = params.one()?;
+    let block_number: BlockNumberOrTag = params.one()?;
 
-    let Some(ref block) = node.lock().unwrap().get_block_by_number(block)? else {
+    let Some(ref block) = node.lock().unwrap().get_block(block_number)? else {
         return Ok(None);
     };
     let miner = node
@@ -75,7 +75,7 @@ fn get_block_details_by_hash(
 ) -> Result<Option<ots::BlockDetails>> {
     let block_hash: B256 = params.one()?;
 
-    let Some(ref block) = node.lock().unwrap().get_block_by_hash(Hash(block_hash.0))? else {
+    let Some(ref block) = node.lock().unwrap().get_block(block_hash)? else {
         return Ok(None);
     };
     let miner = node
@@ -95,13 +95,13 @@ fn get_block_transactions(
     node: &Arc<Mutex<Node>>,
 ) -> Result<Option<ots::BlockTransactions>> {
     let mut params = params.sequence();
-    let block_num: u64 = params.next()?;
+    let block_number: BlockNumberOrTag = params.next()?;
     let page_number: usize = params.next()?;
     let page_size: usize = params.next()?;
 
     let node = node.lock().unwrap();
 
-    let Some(block) = node.get_block_by_number(block_num)? else {
+    let Some(block) = node.get_block(block_number)? else {
         return Ok(None);
     };
     let miner = node.get_proposer_reward_address(block.header)?;
@@ -228,12 +228,13 @@ fn get_transaction_error(params: Params, node: &Arc<Mutex<Node>>) -> Result<Cow<
 fn has_code(params: Params, node: &Arc<Mutex<Node>>) -> Result<bool> {
     let mut params = params.sequence();
     let address: Address = params.next()?;
-    let block_number: BlockNumber = params.next()?;
+    let block_id: BlockId = params.optional_next()?.unwrap_or_default();
 
     let empty = node
         .lock()
         .unwrap()
-        .get_account(address, block_number)?
+        .get_state(block_id)?
+        .get_account(address)?
         .code
         .is_eoa();
 
@@ -291,7 +292,7 @@ fn search_transactions_inner(
         let timestamp = node
             .lock()
             .unwrap()
-            .get_block_by_hash(Hash(txn.block_hash.unwrap_or_default().0))?
+            .get_block(txn.block_hash.unwrap_or_default())?
             .unwrap()
             .timestamp();
 

evm_scilla_js_tests/test/rpc/eth_getBlockByNumber.ts
@@ -129,32 +129,6 @@ describe("Calling " + METHOD, function () {
     });
   });
 
-  it("should get full transactions objects by its block number", async function () {
-    let blockNumber = 0x1;
-
-    await sendJsonRpcRequest(METHOD, 2, [blockNumber, true], (result, status) => {
-      logDebug(result);
-
-      assert.equal(status, 200, "has status code");
-      // validate all returned fields
-      TestResponse(result);
-      assert.equal(+result.result.number, blockNumber, `Block number is not ${blockNumber}`);
-    });
-  });
-
-  it("should get only the hashes of transactions objects by its block number", async function () {
-    let blockNumber = 0x1;
-
-    await sendJsonRpcRequest(METHOD, 2, [blockNumber, false], (result, status) => {
-      logDebug(result);
-
-      assert.equal(status, 200, "has status code");
-      // validate all returned fields
-      TestResponse(result);
-      assert.equal(+result.result.number, blockNumber, `Block number is not ${blockNumber}`);
-    });
-  });
-
   it("should get full transactions objects by its block number, even if number is specified as a string", async function () {
     let blockNumber = "0x1";
 

zilliqa/src/api/erigon.rs
@@ -4,17 +4,17 @@ use anyhow::Result;
 use jsonrpsee::{types::Params, RpcModule};
 
 use super::types::eth;
-use crate::{message::BlockNumber, node::Node};
+use crate::node::Node;
 
 pub fn rpc_module(node: Arc<Mutex<Node>>) -> RpcModule<Arc<Mutex<Node>>> {
     super::declare_module!(node, [("erigon_getHeaderByNumber", get_header_by_number)])
 }
 
 fn get_header_by_number(params: Params, node: &Arc<Mutex<Node>>) -> Result<Option<eth::Block>> {
-    let block: BlockNumber = params.one()?;
+    let block: u64 = params.one()?;
 
     // Erigon headers are a subset of the full block response. We choose to just return the full block.
-    let Some(ref block) = node.lock().unwrap().get_block_by_blocknum(block)? else {
+    let Some(ref block) = node.lock().unwrap().get_block(block)? else {
         return Ok(None);
     };
 

zilliqa/src/api/eth.rs
@@ -3,7 +3,7 @@
 use std::sync::{Arc, Mutex, MutexGuard};
 
 use alloy_consensus::{TxEip1559, TxEip2930, TxLegacy};
-use alloy_eips::eip2930::AccessList;
+use alloy_eips::{eip2930::AccessList, BlockId, BlockNumberOrTag};
 use alloy_primitives::{Address, Bytes, Parity, Signature, TxKind, B256, U256, U64};
 use alloy_rlp::{Decodable, Header};
 use alloy_rpc_types::{
@@ -26,7 +26,7 @@ use super::{
 };
 use crate::{
     crypto::Hash,
-    message::{Block, BlockNumber},
+    message::Block,
     node::Node,
     state::Code,
     time::SystemTime,
@@ -125,11 +125,11 @@ fn call(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
     trace!("call: params: {:?}", params);
     let mut params = params.sequence();
     let call_params: CallParams = params.next()?;
-    let block_number: BlockNumber = params.optional_next()?.unwrap_or(BlockNumber::Latest);
+    let block_id: BlockId = params.optional_next()?.unwrap_or_default();
     expect_end_of_params(&mut params, 1, 2)?;
 
     let ret = node.lock().unwrap().call_contract(
-        block_number,
+        block_id,
         call_params.from,
         call_params.to,
         call_params.data.clone(),
@@ -157,7 +157,7 @@ fn estimate_gas(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
     trace!("estimate_gas: params: {:?}", params);
     let mut params = params.sequence();
     let call_params: CallParams = params.next()?;
-    let block_number: BlockNumber = params.optional_next()?.unwrap_or(BlockNumber::Latest);
+    let block_number: BlockNumberOrTag = params.optional_next()?.unwrap_or_default();
     expect_end_of_params(&mut params, 1, 2)?;
 
     let return_value = node.lock().unwrap().estimate_gas(
@@ -176,27 +176,30 @@ fn estimate_gas(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
 fn get_balance(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
     let mut params = params.sequence();
     let address: Address = params.next()?;
-    let block_number: BlockNumber = params.next()?;
+    let block_id: BlockId = params.next()?;
     expect_end_of_params(&mut params, 2, 2)?;
 
     Ok(node
         .lock()
         .unwrap()
-        .get_native_balance(address, block_number)?
+        .get_state(block_id)?
+        .get_account(address)?
+        .balance
         .to_hex())
 }
 
 fn get_code(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
     let mut params = params.sequence();
     let address: Address = params.next()?;
-    let block_number: BlockNumber = params.next()?;
+    let block_id: BlockId = params.next()?;
     expect_end_of_params(&mut params, 2, 2)?;
 
     // For compatibility with Zilliqa 1, eth_getCode also returns Scilla code if any is present.
     let code = &node
         .lock()
         .unwrap()
-        .get_account(address, block_number)?
+        .get_state(block_id)?
+        .get_account(address)?
         .code;
     // do it this way so the compiler will tell us when another option inevitably
     // turns up and we have to deal with it ..
@@ -218,13 +221,14 @@ fn get_storage_at(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
     let address: Address = params.next()?;
     let position: U256 = params.next()?;
     let position = B256::new(position.to_be_bytes());
-    let block_number: BlockNumber = params.next()?;
+    let block_id: BlockId = params.next()?;
     expect_end_of_params(&mut params, 3, 3)?;
 
     let value = node
         .lock()
         .unwrap()
-        .get_account_storage(address, position, block_number)?;
+        .get_state(block_id)?
+        .get_account_storage(address, position)?;
 
     Ok(value.to_hex())
 }
@@ -233,22 +237,14 @@ fn get_transaction_count(params: Params, node: &Arc<Mutex<Node>>) -> Result<Stri
     trace!("get_transaction_count: params: {:?}", params);
     let mut params = params.sequence();
     let address: Address = params.next()?;
-    let block_number: BlockNumber = params.next()?;
+    let block_id: BlockId = params.next()?;
     expect_end_of_params(&mut params, 3, 3)?;
-
-    trace!(
-        "get_transaction_count resp: {:?}",
-        node.lock()
-            .unwrap()
-            .get_account(address, block_number)?
-            .nonce
-            .to_hex()
-    );
 
     Ok(node
         .lock()
         .unwrap()
-        .get_account(address, block_number)?
+        .get_state(block_id)?
+        .get_account(address)?
         .nonce
         .to_hex())
 }
@@ -260,13 +256,12 @@ fn get_gas_price(params: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
 
 fn get_block_by_number(params: Params, node: &Arc<Mutex<Node>>) -> Result<Option<eth::Block>> {
     let mut params = params.sequence();
-    let block_number: BlockNumber = params.next()?;
+    let block_number: BlockNumberOrTag = params.next()?;
     let full: bool = params.next()?;
     expect_end_of_params(&mut params, 2, 2)?;
 
     let node = node.lock().unwrap();
-    let block = node.get_block_by_blocknum(block_number)?;
-
+    let block = node.get_block(block_number)?;
     let block = block.map(|b| convert_block(&node, &b, full)).transpose()?;
 
     Ok(block)
@@ -280,7 +275,7 @@ fn get_block_by_hash(params: Params, node: &Arc<Mutex<Node>>) -> Result<Option<e
 
     let node = node.lock().unwrap();
     let block = node
-        .get_block_by_hash(Hash(hash.0))?
+        .get_block(hash)?
         .map(|b| convert_block(&node, &b, full))
         .transpose()?;
 
@@ -325,7 +320,7 @@ fn get_block_transaction_count_by_hash(
     expect_end_of_params(&mut params, 1, 1)?;
 
     let node = node.lock().unwrap();
-    let block = node.get_block_by_hash(Hash(hash.0))?;
+    let block = node.get_block(hash)?;
 
     Ok(block.map(|b| b.transactions.len().to_hex()))
 }
@@ -336,11 +331,11 @@ fn get_block_transaction_count_by_number(
 ) -> Result<Option<String>> {
     let mut params = params.sequence();
     // The ethereum RPC spec says this is optional, but it is mandatory in geth and erigon.
-    let block_number: BlockNumber = params.next()?;
+    let block_number: BlockNumberOrTag = params.next()?;
     expect_end_of_params(&mut params, 1, 1)?;
 
     let node = node.lock().unwrap();
-    let block = node.get_block_by_blocknum(block_number)?;
+    let block = node.get_block(block_number)?;
 
     Ok(Some(
         block.map_or(0, |block| block.transactions.len()).to_hex(),
@@ -350,8 +345,8 @@ fn get_block_transaction_count_by_number(
 #[derive(Deserialize, Default)]
 #[serde(default, rename_all = "camelCase")]
 struct GetLogsParams {
-    from_block: Option<BlockNumber>,
-    to_block: Option<BlockNumber>,
+    from_block: Option<BlockNumberOrTag>,
+    to_block: Option<BlockNumberOrTag>,
     address: Option<OneOrMany<Address>>,
 
     /// elements represent an alternative that matches any of the contained topics.
@@ -376,18 +371,18 @@ fn get_logs(params: Params, node: &Arc<Mutex<Node>>) -> Result<Vec<eth::Log>> {
     // Find the range of blocks we care about. This is an iterator of blocks.
     let blocks = match (params.block_hash, params.from_block, params.to_block) {
         (Some(block_hash), None, None) => Either::Left(std::iter::once(Ok(node
-            .get_block_by_hash(Hash(block_hash.0))?
+            .get_block(block_hash)?
             .ok_or_else(|| anyhow!("block not found"))?))),
         (None, from, to) => {
-            let from = node.get_number(from.unwrap_or(BlockNumber::Latest));
-            let to = node.get_number(to.unwrap_or(BlockNumber::Latest));
+            let from = node.resolve_block_number(from.unwrap_or(BlockNumberOrTag::Latest));
+            let to = node.resolve_block_number(to.unwrap_or(BlockNumberOrTag::Latest));
 
             if from > to {
                 return Err(anyhow!("`from` is greater than `to` ({from} > {to})"));
             }
 
             Either::Right((from..=to).map(|number| {
-                node.get_block_by_number(number)?
+                node.get_block(number)?
                     .ok_or_else(|| anyhow!("missing block: {number}"))
             }))
         }
@@ -472,7 +467,7 @@ fn get_transaction_by_block_hash_and_index(
 
     let node = node.lock().unwrap();
 
-    let Some(block) = node.get_block_by_hash(Hash(block_hash.0))? else {
+    let Some(block) = node.get_block(block_hash)? else {
         return Ok(None);
     };
     let Some(txn_hash) = block.transactions.get(index.to::<usize>()) else {
@@ -487,12 +482,12 @@ fn get_transaction_by_block_number_and_index(
     node: &Arc<Mutex<Node>>,
 ) -> Result<Option<eth::Transaction>> {
     let mut params = params.sequence();
-    let block_number: BlockNumber = params.next()?;
+    let block_number: BlockNumberOrTag = params.next()?;
     let index: U64 = params.next()?;
 
     let node = node.lock().unwrap();
 
-    let Some(block) = node.get_block_by_blocknum(block_number)? else {
+    let Some(block) = node.get_block(block_number)? else {
         return Ok(None);
     };
     let Some(txn_hash) = block.transactions.get(index.to::<usize>()) else {
@@ -524,7 +519,7 @@ pub(super) fn get_transaction_inner(
 
     // The block can either be null or some based on whether the tx exists
     let block = if let Some(receipt) = node.get_transaction_receipt(hash)? {
-        node.get_block_by_hash(receipt.block_hash)?
+        node.get_block(receipt.block_hash)?
     } else {
         // Even if it has not been mined, the tx may still be in the mempool and should return
         // a correct tx, with pending/null fields
@@ -554,7 +549,7 @@ pub(super) fn get_transaction_receipt_inner(
         hash, receipt
     );
 
-    let Some(block) = node.get_block_by_hash(receipt.block_hash)? else {
+    let Some(block) = node.get_block(receipt.block_hash)? else {
         warn!("Failed to get block when getting TX receipt! {}", hash);
         return Ok(None);
     };
@@ -833,7 +828,7 @@ async fn subscribe(
                     let block = node
                         .lock()
                         .unwrap()
-                        .get_block_by_hash(receipt.block_hash)?
+                        .get_block(receipt.block_hash)?
                         .ok_or_else(|| anyhow!("missing block"))?;
                     if !filter.filter_block_range(block.number()) {
                         continue 'outer;

zilliqa/src/api/trace.rs
@@ -3,6 +3,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use alloy_eips::BlockNumberOrTag;
 use alloy_primitives::B256;
 use alloy_rpc_types_trace::{
     geth::{GethDebugTracingOptions, TraceResult},
@@ -11,7 +12,7 @@ use alloy_rpc_types_trace::{
 use anyhow::Result;
 use jsonrpsee::{types::Params, RpcModule};
 
-use crate::{crypto::Hash, message::BlockNumber, node::Node};
+use crate::{crypto::Hash, node::Node};
 
 pub fn rpc_module(node: Arc<Mutex<Node>>) -> RpcModule<Arc<Mutex<Node>>> {
     super::declare_module!(
@@ -42,10 +43,10 @@ fn debug_trace_block_by_number(
     node: &Arc<Mutex<Node>>,
 ) -> Result<Vec<TraceResult>> {
     let mut params = params.sequence();
-    let block_number: BlockNumber = params.next()?;
+    let block_number: BlockNumberOrTag = params.next()?;
     let trace_type: Option<GethDebugTracingOptions> = params.next()?;
 
     node.lock()
         .unwrap()
-        .debug_trace_block_by_number(block_number, trace_type.unwrap_or_default())
+        .debug_trace_block(block_number, trace_type.unwrap_or_default())
 }

zilliqa/src/api/types/mod.rs
@@ -1,8 +1,11 @@
 use std::fmt::Display;
 
+use alloy_eips::BlockId;
+use alloy_primitives::B256;
 use serde::{ser::SerializeSeq, Serializer};
 
 use super::to_hex::ToHex;
+use crate::crypto;
 
 pub mod eth;
 pub mod ots;
@@ -56,4 +59,10 @@ where
     S: Serializer,
 {
     serializer.collect_str(value)
+}
+
+impl From<crypto::Hash> for BlockId {
+    fn from(hash: crypto::Hash) -> Self {
+        BlockId::from(B256::from(hash))
+    }
 }

zilliqa/src/api/zil.rs
@@ -6,6 +6,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use alloy_eips::BlockId;
 use alloy_primitives::{Address, B256};
 use anyhow::{anyhow, Result};
 use jsonrpsee::{types::Params, RpcModule};
@@ -20,7 +21,6 @@ use crate::{
     api::types::zil::{CreateTransactionResponse, GetTxResponse, RPCErrorCode},
     crypto::Hash,
     exec::zil_contract_address,
-    message::BlockNumber,
     node::Node,
     schnorr,
     scilla::split_storage_key,
@@ -189,7 +189,7 @@ fn get_transaction(params: Params, node: &Arc<Mutex<Node>>) -> Result<GetTxRespo
     let block = node
         .lock()
         .unwrap()
-        .get_block_by_hash(receipt.block_hash)?
+        .get_block(receipt.block_hash)?
         .ok_or_else(|| anyhow!("block does not exist"))?;
 
     GetTxResponse::new(tx, receipt, block.number())
@@ -199,14 +199,11 @@ fn get_balance(params: Params, node: &Arc<Mutex<Node>>) -> Result<Value> {
     let address: Address = params.one()?;
 
     let node = node.lock().unwrap();
-
-    let balance = node.get_native_balance(address, BlockNumber::Latest)?;
+    let account = node.get_state(BlockId::latest())?.get_account(address)?;
     // We need to scale the balance from units of (10^-18) ZIL to (10^-12) ZIL. The value is truncated in this process.
-    let balance = balance / 10u128.pow(6);
-    let balance = balance.to_string();
-    let nonce = node.get_account(address, BlockNumber::Latest)?.nonce;
+    let balance = account.balance / 10u128.pow(6);
 
-    Ok(json!({"balance": balance, "nonce": nonce}))
+    Ok(json!({"balance": balance.to_string(), "nonce": account.nonce}))
 }
 
 fn get_current_mini_epoch(_: Params, node: &Arc<Mutex<Node>>) -> Result<String> {
@@ -216,7 +213,7 @@ fn get_current_mini_epoch(_: Params, node: &Arc<Mutex<Node>>) -> Result<String> 
 fn get_latest_tx_block(_: Params, node: &Arc<Mutex<Node>>) -> Result<zil::TxBlock> {
     let node = node.lock().unwrap();
     let block = node
-        .get_block_by_number(node.get_number(BlockNumber::Latest))?
+        .get_block(BlockId::latest())?
         .ok_or_else(|| anyhow!("no blocks"))?;
 
     Ok((&block).into())
@@ -283,7 +280,8 @@ fn get_smart_contract_state(params: Params, node: &Arc<Mutex<Node>>) -> Result<V
     let node = node.lock().unwrap();
 
     // First get the account and check that its a scilla account
-    let account = node.get_account(address, BlockNumber::Latest)?;
+    let state = node.get_state(BlockId::latest())?;
+    let account = state.get_account(address)?;
 
     let result = json!({
         "_balance": ZilAmount::from_amount(account.balance).to_string(),
@@ -294,7 +292,6 @@ fn get_smart_contract_state(params: Params, node: &Arc<Mutex<Node>>) -> Result<V
 
     let is_scilla = account.code.scilla_code_and_init_data().is_some();
     if is_scilla {
-        let state = node.state_at(BlockNumber::Latest)?;
         let trie = state.get_account_trie(address)?;
         for (k, v) in trie.iter() {
             let (var_name, indices) = split_storage_key(&k)?;
@@ -317,7 +314,9 @@ fn get_smart_contract_state(params: Params, node: &Arc<Mutex<Node>>) -> Result<V
 fn get_smart_contract_code(params: Params, node: &Arc<Mutex<Node>>) -> Result<Value> {
     let smart_contract_address: Address = params.one()?;
     let node = node.lock().unwrap();
-    let account = node.get_account(smart_contract_address, BlockNumber::Latest)?;
+    let account = node
+        .get_state(BlockId::latest())?
+        .get_account(smart_contract_address)?;
 
     let Some((code, _)) = account.code.scilla_code_and_init_data() else {
         return Err(anyhow!("Address not contract address"));
@@ -329,7 +328,9 @@ fn get_smart_contract_code(params: Params, node: &Arc<Mutex<Node>>) -> Result<Va
 fn get_smart_contract_init(params: Params, node: &Arc<Mutex<Node>>) -> Result<Value> {
     let smart_contract_address: Address = params.one()?;
     let node = node.lock().unwrap();
-    let account = node.get_account(smart_contract_address, BlockNumber::Latest)?;
+    let account = node
+        .get_state(BlockId::latest())?
+        .get_account(smart_contract_address)?;
 
     let Some((_, init_data)) = account.code.scilla_code_and_init_data() else {
         return Err(anyhow!("Address not contract address"));
@@ -346,7 +347,7 @@ fn get_transactions_for_tx_block(
     let block_number: u64 = block_number.parse()?;
 
     let node = node.lock().unwrap();
-    let Some(block) = node.get_block_by_number(block_number)? else {
+    let Some(block) = node.get_block(block_number)? else {
         return Err(anyhow!("Tx Block does not exist"));
     };
     if block.transactions.is_empty() {
@@ -372,7 +373,7 @@ fn get_tx_block(
     let block_number: u64 = block_number.parse()?;
 
     let node = node.lock().unwrap();
-    let Some(block) = node.get_block_by_number(block_number)? else {
+    let Some(block) = node.get_block(block_number)? else {
         return Ok(None);
     };
     let mut block: zil::TxBlock = (&block).into();
@@ -395,7 +396,8 @@ fn get_smart_contracts(params: Params, node: &Arc<Mutex<Node>>) -> Result<Vec<Sm
     let nonce = node
         .lock()
         .unwrap()
-        .get_account(address, BlockNumber::Latest)?
+        .get_state(BlockId::latest())?
+        .get_account(address)?
         .nonce;
 
     let mut contracts = vec![];
@@ -406,7 +408,8 @@ fn get_smart_contracts(params: Params, node: &Arc<Mutex<Node>>) -> Result<Vec<Sm
         let is_scilla = node
             .lock()
             .unwrap()
-            .get_account(contract_address, BlockNumber::Latest)?
+            .get_state(BlockId::latest())?
+            .get_account(contract_address)?
             .code
             .scilla_code_and_init_data()
             .is_some();

zilliqa/src/message.rs
@@ -2,13 +2,12 @@ use std::{
     collections::HashSet,
     fmt,
     fmt::{Display, Formatter},
-    str::FromStr,
 };
 
 use alloy_primitives::Address;
 use anyhow::{anyhow, Result};
 use bitvec::{bitvec, order::Msb0};
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use sha3::{Digest, Keccak256};
 
 use crate::{
@@ -424,94 +423,6 @@ impl Default for BlockHeader {
             state_root_hash: Hash(Keccak256::digest([alloy_rlp::EMPTY_STRING_CODE]).into()),
             timestamp: SystemTime::UNIX_EPOCH,
             gas_used: EvmGas(0),
-        }
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub enum BlockNumber {
-    Number(u64),
-    Earliest,
-    Latest,
-    Safe,
-    Finalized,
-    Pending,
-}
-
-impl Display for BlockNumber {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Self::Number(num) => num.to_string(),
-                Self::Earliest => "earliest".to_string(),
-                Self::Latest => "latest".to_string(),
-                Self::Safe => "safe".to_string(),
-                Self::Finalized => "finalized".to_string(),
-                Self::Pending => "pending".to_string(),
-            }
-        )
-    }
-}
-
-impl From<u64> for BlockNumber {
-    fn from(num: u64) -> Self {
-        Self::Number(num)
-    }
-}
-
-impl<'de> Deserialize<'de> for BlockNumber {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct Visitor;
-
-        impl<'de> serde::de::Visitor<'de> for Visitor {
-            type Value = BlockNumber;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                write!(formatter, "a non-negative integer or a string")
-            }
-
-            fn visit_u64<E>(self, val: u64) -> Result<Self::Value, E>
-            where
-                E: serde::de::Error,
-            {
-                Ok(BlockNumber::Number(val))
-            }
-
-            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-            where
-                E: serde::de::Error,
-            {
-                v.parse().map_err(serde::de::Error::custom)
-            }
-        }
-
-        deserializer.deserialize_any(Visitor)
-    }
-}
-
-impl FromStr for BlockNumber {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "earliest" => Ok(BlockNumber::Earliest),
-            "latest" => Ok(BlockNumber::Latest),
-            "safe" => Ok(BlockNumber::Safe),
-            "finalized" => Ok(BlockNumber::Finalized),
-            "pending" => Ok(BlockNumber::Pending),
-            number => {
-                if let Some(number) = number.strip_prefix("0x") {
-                    let number = u64::from_str_radix(number, 16)?;
-                    Ok(BlockNumber::Number(number))
-                } else {
-                    Err(anyhow!("invalid block number: {s}"))
-                }
-            }
         }
     }
 }

commit 94b328b586ddbcc87e7fc1d6ac3eb739c1c20468

Fix parameter types for `ots_getBlock{Details,Transactions}`
I broke these in #1079. Turns out they should only accept a JSON
number, not a string.

zilliqa/src/api/ots.rs
@@ -51,7 +51,7 @@ pub fn get_otterscan_api_level(_: Params, _: &Arc<Mutex<Node>>) -> Result<u64> {
 }
 
 fn get_block_details(params: Params, node: &Arc<Mutex<Node>>) -> Result<Option<ots::BlockDetails>> {
-    let block_number: BlockNumberOrTag = params.one()?;
+    let block_number: u64 = params.one()?;
 
     let Some(ref block) = node.lock().unwrap().get_block(block_number)? else {
         return Ok(None);
@@ -95,7 +95,7 @@ fn get_block_transactions(
     node: &Arc<Mutex<Node>>,
 ) -> Result<Option<ots::BlockTransactions>> {
     let mut params = params.sequence();
-    let block_number: BlockNumberOrTag = params.next()?;
+    let block_number: u64 = params.next()?;
     let page_number: usize = params.next()?;
     let page_size: usize = params.next()?;
 