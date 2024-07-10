bb5aa2359446679f7613cdd8b950986168893235
saeed-zil <saeed@zilliqa.com>
2024-07-10 12:25:27 +0330

    Add read type.

--- z2/src/kpi/read.rs
+++ z2/src/kpi/read.rs
@@ -10,21 +10,27 @@ use super::{
 
 impl ScenarioAgent for CallReadOnlyMethods {
     async fn run(&self, config: &Config) -> Result<KpiResult> {
-        let provider = config.get_provider()?;
-        let mut total_latency = 0.0;
-        let num_requests = self.iterations;
+        match self.r#type {
+            super::config::CallReadOnlyMethodsType::GetBalance => {
+                let provider = config.get_provider()?;
+                let mut total_latency = 0.0;
+                let num_requests = self.iterations;
+                for _ in 0..num_requests {
+                    let wallet = LocalWallet::create_random()?;
+                    let start = Instant::now();
+                    let _ = provider.get_balance(&wallet.address).await?;
+                    let duration = start.elapsed();
+                    total_latency += duration.as_secs_f64();
+                }
 
-        for _ in 0..num_requests {
-            let wallet = LocalWallet::create_random()?;
-            let start = Instant::now();
-            let _ = provider.get_balance(&wallet.address).await?;
-            let duration = start.elapsed();
-            total_latency += duration.as_secs_f64();
+                let average_latency = total_latency / num_requests as f64;
+                Ok(KpiResult::CallReadOnlyMethods(CallReadOnlyMethodsResult {
+                    latency: average_latency,
+                }))
+            }
+            super::config::CallReadOnlyMethodsType::QueryContractSubState => todo!(),
+            super::config::CallReadOnlyMethodsType::EvmContractViewFunction => todo!(),
+            super::config::CallReadOnlyMethodsType::Mixed => todo!(),
         }
-
-        let average_latency = total_latency / num_requests as f64;
-        Ok(KpiResult::CallReadOnlyMethods(CallReadOnlyMethodsResult {
-            latency: average_latency,
-        }))
     }
 }
