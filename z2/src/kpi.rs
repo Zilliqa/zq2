1da6474168c3b7531ae8709f07277a393cab6380
saeed-zil <saeed@zilliqa.com>
2024-07-11 13:35:07 +0330

    Add block info collector task.

--- z2/src/kpi.rs
+++ z2/src/kpi.rs
@@ -1,6 +1,10 @@
 use anyhow::Result;
 pub use config::Config;
+use ethers::providers::{Middleware, Provider, Ws};
+use futures::StreamExt;
+use primitive_types::U256;
 use serde::Serialize;
+use tokio::sync::oneshot;
 
 mod account;
 pub mod config;
@@ -8,15 +12,17 @@ mod read;
 mod txn;
 
 use account::Account;
+use tokio::task::JoinHandle;
 
 #[derive(Debug, Serialize)]
-pub enum KpiResult {
+enum KpiResult {
     CallReadOnlyMethods(CallReadOnlyMethodsResult),
     SendTransaction(CallTransactionResult),
+    BlockInfo(Vec<BlockInfo>),
 }
 
 #[derive(Debug, Serialize)]
-pub struct CallTransactionResult {
+struct CallTransactionResult {
     latency: f64,
     throughput: f64,
     success_rate: f32,
@@ -24,7 +30,7 @@ pub struct CallTransactionResult {
 }
 
 #[derive(Debug, Serialize)]
-pub struct CallReadOnlyMethodsResult {
+struct CallReadOnlyMethodsResult {
     latency: f64,
 }
 
@@ -34,9 +40,56 @@ trait ScenarioAgent {
 
 pub struct Kpi;
 
+#[derive(Debug, Serialize)]
+struct BlockInfo {
+    timestamp_delta: U256,
+    num_transactions: usize,
+    gas_used: u64,
+}
+
+fn spawn_block_info_collector(
+    mut rx: Option<oneshot::Receiver<()>>,
+) -> JoinHandle<Result<Vec<BlockInfo>>> {
+    tokio::spawn(async move {
+        let provider = Provider::<Ws>::connect("ws://localhost:4201").await?;
+        let mut stream = provider.subscribe_blocks().await?;
+        let mut previous_timestamp = None;
+        let mut block_infos = Vec::new();
+
+        loop {
+            tokio::select! {
+                block =  stream.next() => {
+                    if let Some(block) = block {
+                        let timestamp_delta = block.timestamp - previous_timestamp.unwrap_or(block.timestamp);
+                        previous_timestamp = Some(block.timestamp);
+
+                        let num_transactions = block.transactions.len();
+                        let gas_used = block.gas_used;
+
+                        block_infos.push(BlockInfo {
+                            timestamp_delta,
+                            num_transactions,
+                            gas_used: gas_used.as_u64(),
+                        });
+                    }
+                }
+                _ = rx.as_mut().unwrap() => {
+                    break;
+                }
+            }
+        }
+
+        Ok(block_infos)
+    })
+}
+
 impl Kpi {
     pub async fn run(config: &Config) {
         let mut results = vec![];
+
+        let (tx, rx) = oneshot::channel();
+        let task_handle = spawn_block_info_collector(Some(rx));
+
         for agent in config.scenario.iter() {
             let out = match agent {
                 config::ScenarioStep::CallReadOnlyMethods(r) => r.run(config).await,
@@ -46,6 +99,11 @@ impl Kpi {
 
             results.push(out);
         }
+
+        let _ = tx.send(());
+
+        let block_results = task_handle.await.unwrap().unwrap();
+        results.push(KpiResult::BlockInfo(block_results));
 
         let output = serde_json::to_string_pretty(&results).unwrap();
 
