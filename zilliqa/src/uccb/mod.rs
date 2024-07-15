01063cacb086c7be3f4653d518b4e67df4534a8d
Yaron <yaron@zilliqa.com>
2024-07-15 14:56:01 +0800

    Add a generic function to read UCCB's config.

--- zilliqa/src/uccb/mod.rs
+++ zilliqa/src/uccb/mod.rs
@@ -1,2 +1,22 @@
+use anyhow::{anyhow, Result};
+use crate::crypto::SecretKey;
+use std::{fs, path::PathBuf};
+
 pub mod cfg;
+
+pub trait Args {
+    fn secret_key(&self) -> &SecretKey;
+    fn config_file(&self) -> &PathBuf;
+}
+pub fn read_config<T: Args>(args: &T) -> Result<cfg::Config> {
+    let config_file = &args.config_file();
+    let config_content = if config_file.exists() {
+        fs::read_to_string(&config_file)?
+    }
+    else {
+        panic!("Please specify a config file");
+    };
+
+    Ok(toml::from_str(&config_content)?)
+}
 
