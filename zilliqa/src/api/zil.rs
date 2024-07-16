40b1ccbed30484b9e712a21b7641b02be7bffdc4
joseph-zilliqa <joseph@zilliqa.com>
2024-07-16 08:46:30 +0000

    Fix GetBalance API call to return error if account does not exist (#1159)
    * Rename State::get_account to State::get_account_or_default
    
    * Fix GetBalance API call to return error if account does not exist
    
    * Fix GetBalance RPC call to return an error if the supplied account does not exist
    
    * Change GetBalance RPC to use State::has_account
    
    * Change `GetBalance` RPC to use `State::has_account`
    
    * Remove `get_account`
    
    * Rename `get_account_or_default` back to `get_account`

--- zilliqa/src/api/zil.rs
+++ zilliqa/src/api/zil.rs
@@ -202,7 +202,20 @@ fn get_balance(params: Params, node: &Arc<Mutex<Node>>) -> Result<Value> {
     let block = node
         .get_block(BlockId::latest())?
         .ok_or_else(|| anyhow!("Unable to get latest block!"))?;
-    let account = node.get_state(&block)?.get_account(address)?;
+
+    let state = node.get_state(&block)?;
+
+    if !state.has_account(address)? {
+        return Err(jsonrpsee::types::ErrorObject::owned(
+            RPCErrorCode::RpcInvalidAddressOrKey as i32,
+            "Account is not created",
+            None::<()>,
+        )
+        .into());
+    }
+
+    let account = state.get_account(address)?;
+
     // We need to scale the balance from units of (10^-18) ZIL to (10^-12) ZIL. The value is truncated in this process.
     let balance = account.balance / 10u128.pow(6);
 
