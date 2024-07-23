81df7d3527fa654dec431b4a8ad9cc5017ee02a3
Richard Watts <108257153+rrw-zilliqa@users.noreply.github.com>
2024-07-23 20:05:31 +0100

    Add remaining eth_ API docs. (#1141)
    * (feat) Document getTransactionByBlockHashAndIndex
    
    * (feat) Incidentally add a definition to the converter notes.
    
    * (fix) Fix spelling of default function
    (fix) Add comments on undocumented config variables so folks know what to set them to.
    
    * (feat) document eth_getTransactionByBlockNumberAndIndex
    
    * (fix) Amend docs to indicate that blocks not known to this node will get a `null` response (eg. for light nodes)
    (feat) Document eth_getTransactionByHash
    
    * (feat) WIP docs.
    
    * (fix) getTransactionReceipt now documented.
    
    * (feat) Document remaining eth_ calls

--- zilliqa/src/cfg.rs
+++ zilliqa/src/cfg.rs
@@ -39,7 +39,7 @@ pub struct Config {
 #[serde(deny_unknown_fields)]
 pub struct NodeConfig {
     /// The port to listen for JSON-RPC requests on. Defaults to 4201.
-    #[serde(default = "json_rcp_port_default")]
+    #[serde(default = "json_rpc_port_default")]
     pub json_rpc_port: u16,
     /// If true, the JSON-RPC server is not started. Defaults to false.
     #[serde(default = "disable_rpc_default")]
@@ -107,7 +107,7 @@ pub fn allowed_timestamp_skew_default() -> Duration {
     Duration::from_secs(10)
 }
 
-pub fn json_rcp_port_default() -> u16 {
+pub fn json_rpc_port_default() -> u16 {
     4201
 }
 
@@ -208,14 +208,23 @@ pub struct ConsensusConfig {
     /// `--add-host host.docker.internal:host-gateway` to Docker and set this to `host.docker.internal`.
     #[serde(default = "local_address_default")]
     pub local_address: String,
+    /// Reward amount issued per hour, in Wei.
     pub rewards_per_hour: Amount,
+    /// Number of blocks per hour. The reward per block is set at (rewards_per_hour/blocks_per_hour) Wei.
     pub blocks_per_hour: u64,
+    /// The minimum stake passed into the deposit contract constructor at genesis, in Wei. Subsequent changes to this
+    /// parameter will have no effect. You must not use this parameter at run-time; obtain the minimum stake in effect
+    /// from the contract - otherwise if this value ever changes, there will be a mismatch between what the deposit
+    /// contract believes and what the validators believe to be the minimum stake.
     pub minimum_stake: Amount,
+    /// Maximum amount of gas permitted in a single block; any transactions over this
+    /// will be held until the next block. The white paper specifies this as 84_000_000.
     pub eth_block_gas_limit: EvmGas,
     #[serde(default = "blocks_per_epoch_default")]
     pub blocks_per_epoch: u64,
     #[serde(default = "epochs_per_checkpoint_default")]
     pub epochs_per_checkpoint: u64,
+    /// The gas price, in Wei per unit of EVM gas.
     pub gas_price: Amount,
 }
 
--- zilliqa/src/consensus.rs
+++ zilliqa/src/consensus.rs
@@ -629,6 +629,8 @@ impl Consensus {
         Ok(None)
     }
 
+    /// Note that the algorithm below is mentioned in cfg.rs - if you change the way
+    /// rewards are calculated, please change the comments in the configuration structure there.
     fn apply_rewards(
         &mut self,
         committee: &[NodePublicKey],
