8c518960b4d685570be31d77464bbf90d5dacea1
theo-zil <theodore@zilliqa.com>
2024-07-29 10:03:43 +0100

    typo
    whoops...
    
    Co-authored-by: James Hinshelwood <JamesHinshelwood@users.noreply.github.com>

--- infra/tf/locals.tf
+++ infra/tf/locals.tf
@@ -5,7 +5,7 @@
 locals {
   binary_location      = "${path.module}/../../target/x86_64-unknown-linux-gnu/release/zilliqa"
   labels               = merge(var.labels, { "zq2-network" = var.network_name })
-  bootstrap_public_key = data.external.bootstrap_key_converted.result.bsl_public_key
+  bootstrap_public_key = data.external.bootstrap_key_converted.result.bls_public_key
   bootstrap_peer_id    = data.external.bootstrap_key_converted.result.peer_id
   genesis_address      = data.external.genesis_key_converted.result.address
   network_name         = element(split("/", data.google_compute_subnetwork.default.network), length(split("/", data.google_compute_subnetwork.default.network)) - 1)
