2640160c247c43d90cfbf7f7e1d5de2b1a73444c
Yaron <yaron@zilliqa.com>
2024-07-31 13:00:45 +0800

    Store the ABI of the deploy contract.

--- zilliqa/src/uccb/client.rs
+++ zilliqa/src/uccb/client.rs
@@ -1,22 +1,14 @@
-use std::sync::Arc;
-
+use crate::uccb::cfg::ChainConfig;
 use alloy::{
     network::Ethereum,
     primitives::Address,
     providers::{fillers::*, Identity, Provider, ProviderBuilder, RootProvider, WsConnect},
     pubsub::PubSubFrontend,
+    signers::local::PrivateKeySigner,
 };
-
 use anyhow::Result;
-use ethers::{
-    signers::LocalWallet,
-    // types::{Address, U256},
-};
+use std::sync::Arc;
 
-use crate::uccb::cfg::ChainConfig;
-
-// pub type Client =
-// NonceManagerMiddleware<SignerMiddleware<ethers::providers::Provider<Ws>, LocalWallet>>;
 pub type ChainProvider = FillProvider<
     JoinFill<JoinFill<JoinFill<Identity, GasFiller>, NonceFiller>, ChainIdFiller>,
     RootProvider<PubSubFrontend>,
@@ -27,11 +19,11 @@ pub type ChainProvider = FillProvider<
 #[derive(Debug, Clone)]
 pub struct ChainClient {
     pub rpc_url: String,
-    pub client: Arc<ChainProvider>,
+    pub provider: Arc<ChainProvider>,
     pub validator_manager_address: Address,
     // pub chain_gateway_address: Address,
     pub chain_id: u64,
-    pub wallet: LocalWallet,
+    pub signer: PrivateKeySigner,
     pub chain_gateway_block_deployed: u64,
     pub block_instant_finality: bool,
     pub legacy_gas_estimation: bool,
@@ -42,7 +34,7 @@ impl ChainClient {
         config: &ChainConfig,
         validator_manager_address: Address,
         // chain_gateway_address: Address,
-        wallet: LocalWallet,
+        signer: PrivateKeySigner,
     ) -> Result<Self> {
         let ws = WsConnect::new(&config.rpc_url);
         let provider = ProviderBuilder::new()
@@ -50,19 +42,19 @@ impl ChainClient {
             .on_ws(ws)
             .await?;
         let chain_id = provider.get_chain_id().await?;
-        let client: Arc<ChainProvider> = Arc::new(provider);
+        let provider: Arc<ChainProvider> = Arc::new(provider);
 
         // TODO: get the validator_manager_address from chain_gateway itself
-        // let chain_gateway = ChainGateway::new(config.chain_gateway_address, client.clone());
+        // let chain_gateway = ChainGateway::new(config.chain_gateway_address, provider.clone());
         // let validator_manager_address: Address = chain_gateway.validator_manager().call().await?;
 
         Ok(ChainClient {
             rpc_url: config.rpc_url.clone(),
-            client,
+            provider,
             validator_manager_address,
             // chain_gateway_address,
             chain_id,
-            wallet,
+            signer,
             chain_gateway_block_deployed: config.chain_gateway_block_deployed,
             block_instant_finality: config.block_instant_finality.unwrap_or_default(),
             legacy_gas_estimation: config.legacy_gas_estimation.unwrap_or_default(),
