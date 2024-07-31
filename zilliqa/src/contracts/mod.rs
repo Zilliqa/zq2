f86f4ba7ab1e8d1d36ddb9cfcaeaf59c0bc66306
Yaron <yaron@zilliqa.com>
2024-07-31 13:00:45 +0800

    Store the ABI of the deploy contract.

--- zilliqa/src/contracts/mod.rs
+++ zilliqa/src/contracts/mod.rs
@@ -12,6 +12,7 @@ pub mod deposit {
         Lazy::new(|| CONTRACT.abi.constructor().unwrap().clone());
 
     pub static BYTECODE: Lazy<Vec<u8>> = Lazy::new(|| CONTRACT.bytecode.clone());
+    pub static ABI: Lazy<ethabi::Contract> = Lazy::new(|| CONTRACT.abi.clone());
     pub static LEADER_AT_VIEW: Lazy<Function> =
         Lazy::new(|| CONTRACT.abi.function("leaderAtView").unwrap().clone());
     pub static TEMP_REMOVE_STAKER: Lazy<Function> =
