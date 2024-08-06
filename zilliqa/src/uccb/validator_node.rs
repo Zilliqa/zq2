72481786f1e8cd35d9a02eab7f15c4b1a1f72c2d
Yaron <yaron@zilliqa.com>
2024-08-07 00:17:56 +0800

    Start listening to events.

--- zilliqa/src/uccb/validator_node.rs
+++ zilliqa/src/uccb/validator_node.rs
@@ -1,6 +1,14 @@
 use std::{collections::HashMap, str::FromStr, time::Duration};
 
-use alloy::{primitives::U256, signers::local::PrivateKeySigner};
+use alloy::{
+    contract::{ContractInstance, DynCallBuilder, Interface},
+    dyn_abi::DynSolValue,
+    eips::{eip2930::AccessList, BlockNumberOrTag},
+    primitives::U256,
+    pubsub::PubSubFrontend,
+    rpc::types::Filter,
+    signers::local::PrivateKeySigner,
+};
 use anyhow::Result;
 use futures_util::StreamExt;
 use libp2p::PeerId;
@@ -13,13 +21,13 @@ use tokio_stream::wrappers::UnboundedReceiverStream;
 use tracing::{error, info, warn};
 
 use crate::{
-    crypto::SecretKey,
     message::ExternalMessage,
     p2p_node::OutboundMessageTuple,
     uccb::{
         bridge_node::BridgeNode,
         cfg::Config,
         client::{ChainClient, ChainProvider /*, ContractInitializer*/},
+        contracts,
         // contracts::{ChainGateway, ChainGatewayErrors},
         message::{Dispatch, InboundBridgeMessage, OutboundBridgeMessage},
         signature::SignatureTracker,
@@ -30,7 +38,6 @@ type ChainID = U256;
 
 #[derive(Debug)]
 pub struct ValidatorNode {
-    peer_id: PeerId,
     shard_id: u64,
     /// The following two message streams are used for networked messages.
     /// The sender is provided to the p2p coordinator, to forward messages to the node.
@@ -60,7 +67,6 @@ impl ValidatorNode {
             UnboundedReceiverStream::new(bridge_inbound_message_receiver);
 
         Ok(ValidatorNode {
-            peer_id,
             shard_id,
             bridge_outbound_message_sender,
             bridge_inbound_message_sender,
@@ -110,12 +116,10 @@ impl ValidatorNode {
                     // forward messages to bridge_chain_node
                     match message {
                         ExternalMessage::BridgeEcho(echo) => {
-                            /*
                             // Send echo to respective source_chain_id to be verified, only if chain is supported
                             if let Some(sender) = bridge_node_message_senders.get(&echo.event.source_chain_id) {
                                 sender.send(InboundBridgeMessage::Relay(echo))?;
                             }
-                            */
                         },
                         _ => {
                             warn!("Unexpected message");
@@ -126,9 +130,7 @@ impl ValidatorNode {
                     match message {
                         OutboundBridgeMessage::Dispatch(dispatch) => {
                             // Send relay event to target chain
-                            /*
                             self.dispatch_message(dispatch).await?;
-                            */
                         },
                         OutboundBridgeMessage::Dispatched(dispatched) => {
                             // Forward message to another chain_node
@@ -159,104 +161,137 @@ impl ValidatorNode {
         }
     }
 
-    /*
     async fn dispatch_message(&self, dispatch: Dispatch) -> Result<()> {
         let Dispatch {
             event, signatures, ..
         } = dispatch;
 
-        let client = match self.chain_clients.get(&event.target_chain_id) {
-            Some(client) => client,
+        let chain_client = match self.chain_clients.get(&event.target_chain_id) {
+            Some(chain_client) => chain_client,
             None => {
                 warn!("Unsupported Chain ID");
                 return Ok(());
             }
         };
 
-        let chain_gateway: ChainGateway<ChainProvider> = client.get_contract();
+        //if let Some(relay_event) = contracts::chain_gateway::ABI.event("relay") {
+        let chain_gateway_contract: ContractInstance<PubSubFrontend, _> =
+            contracts::chain_gateway::instance(
+                chain_client.chain_gateway_address,
+                chain_client.provider.as_ref(),
+            );
 
-        let function_call = chain_gateway.dispatch(
-            event.source_chain_id,
-            event.target,
-            event.call,
-            event.gas_limit,
-            event.nonce,
-            signatures.into_ordered_signatures(),
-        );
-        info!(
-            "Preparing to send dispatch {}.{}",
-            event.target_chain_id, event.nonce
-        );
+        /*
+        pub source_chain_id: U256,
+        pub target_chain_id: U256,
+        pub target: Address,
+        pub call: Bytes,
+        pub gas_limit: U256,
+        pub nonce: U256,
+                     */
+        let args = vec![
+            DynSolValue::Uint(event.source_chain_id, 256),
+            DynSolValue::Address(event.target),
+            DynSolValue::Bytes(event.call.to_vec()),
+            DynSolValue::Uint(event.gas_limit, 256),
+            DynSolValue::Uint(event.nonce, 256),
+            // TODO: signatures
+            DynSolValue::Array(vec![]),
+        ];
+        let call_builder = chain_gateway_contract.function("dispatch", &args)?;
+        let output = call_builder.send().await?;
+        let receipt = output.get_receipt().await?;
+        info!("Receipt from {}: {receipt:?}", &chain_client.rpc_url);
 
-        let function_call = if client.legacy_gas_estimation {
-            function_call.legacy()
-        } else {
-            function_call
-        };
+        //chain_gateway_contract.event(filter)
+        //} else {
+        //}
 
-        for i in 1..6 {
-            info!("Dispatch Attempt {:?}", i);
+        /*
+            let chain_gateway: ChainGateway<ChainProvider> = client.get_contract();
 
-            // Get gas estimate
-            // TODO: refactor configs specifically for zilliqa
-            let _function_call = if client.legacy_gas_estimation {
-                let gas_estimate = match function_call.estimate_gas().await {
-                    Ok(estimate) => estimate,
-                    Err(err) => {
-                        warn!("Failed to estimate gas, {:?}", err);
-                        return Ok(());
-                    }
-                };
-                info!("Gas estimate {:?}", gas_estimate);
-                function_call.clone().gas(gas_estimate * 130 / 100) // Apply multiplier
+            let function_call = chain_gateway.dispatch(
+                event.source_chain_id,
+                event.target,
+                event.call,
+                event.gas_limit,
+                event.nonce,
+                signatures.into_ordered_signatures(),
+            );
+            info!(
+                "Preparing to send dispatch {}.{}",
+                event.target_chain_id, event.nonce
+            );
+
+            let function_call = if client.legacy_gas_estimation {
+                function_call.legacy()
             } else {
-                let function_call = function_call.clone();
-                // `eth_call` does not seem to work on ZQ so it had to be skipped
-                // Simulate call, if fails decode error and exit early
-                if let Err(contract_err) = function_call.call().await {
-                    match contract_err.decode_contract_revert::<ChainGatewayErrors>() {
-                        Some(ChainGatewayErrors::AlreadyDispatched(_)) => {
-                            info!(
-                                "Already Dispatched {}.{}",
-                                event.target_chain_id, event.nonce
-                            );
-                            return Ok(());
-                        }
-                        Some(err) => {
-                            warn!("ChainGatewayError: {:?}", err);
-                            return Ok(());
-                        }
-                        None => {
-                            warn!("Some unknown error, {:?}", contract_err);
-                            tokio::time::sleep(Duration::from_secs(1)).await;
-                            continue;
-                        }
-                    }
-                }
                 function_call
             };
 
-            // Make the actual call
-            match _function_call.send().await {
-                Ok(tx) => {
-                    println!(
-                        "Transaction Sent {}.{} {:?}",
-                        event.target_chain_id,
-                        event.nonce,
-                        tx.tx_hash()
-                    );
+            for i in 1..6 {
+                info!("Dispatch Attempt {:?}", i);
 
-                    return Ok(());
+                // Get gas estimate
+                // TODO: refactor configs specifically for zilliqa
+                let _function_call = if client.legacy_gas_estimation {
+                    let gas_estimate = match function_call.estimate_gas().await {
+                        Ok(estimate) => estimate,
+                        Err(err) => {
+                            warn!("Failed to estimate gas, {:?}", err);
+                            return Ok(());
+                        }
+                    };
+                    info!("Gas estimate {:?}", gas_estimate);
+                    function_call.clone().gas(gas_estimate * 130 / 100) // Apply multiplier
+                } else {
+                    let function_call = function_call.clone();
+                    // `eth_call` does not seem to work on ZQ so it had to be skipped
+                    // Simulate call, if fails decode error and exit early
+                    if let Err(contract_err) = function_call.call().await {
+                        match contract_err.decode_contract_revert::<ChainGatewayErrors>() {
+                            Some(ChainGatewayErrors::AlreadyDispatched(_)) => {
+                                info!(
+                                    "Already Dispatched {}.{}",
+                                    event.target_chain_id, event.nonce
+                                );
+                                return Ok(());
+                            }
+                            Some(err) => {
+                                warn!("ChainGatewayError: {:?}", err);
+                                return Ok(());
+                            }
+                            None => {
+                                warn!("Some unknown error, {:?}", contract_err);
+                                tokio::time::sleep(Duration::from_secs(1)).await;
+                                continue;
+                            }
+                        }
+                    }
+                    function_call
+                };
+
+                // Make the actual call
+                match _function_call.send().await {
+                    Ok(tx) => {
+                        println!(
+                            "Transaction Sent {}.{} {:?}",
+                            event.target_chain_id,
+                            event.nonce,
+                            tx.tx_hash()
+                        );
+
+                        return Ok(());
+                    }
+                    Err(err) => {
+                        warn!("Failed to send: {:?}", err);
+                    }
                 }
-                Err(err) => {
-                    warn!("Failed to send: {:?}", err);
-                }
+
+                tokio::time::sleep(Duration::from_secs(1)).await;
             }
-
-            tokio::time::sleep(Duration::from_secs(1)).await;
-        }
+        */
 
         Ok(())
     }
-    */
 }
