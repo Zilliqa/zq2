800e8bdbf8f71c62ed3504b1e0bc97230c261d30
Yaron <yaron@zilliqa.com>
2024-08-06 15:45:50 +0800

    Migrate more Solidity scripts.

--- uccb/contracts/src/core/ValidatorManager.sol
+++ uccb/contracts/src/core/ValidatorManager.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {SignatureValidator} from "contracts/core/SignatureValidator.sol";
 
 interface IValidatorManager {
     function addValidator(address user) external returns (bool);
@@ -14,10 +15,16 @@ interface IValidatorManager {
     function isValidator(address user) external view returns (bool);
 
     function validatorsSize() external view returns (uint);
+
+    function validateMessageWithSupermajority(
+        bytes32 ethSignedMessageHash,
+        bytes[] calldata signatures
+    ) external view;
 }
 
 contract ValidatorManager is Ownable2Step {
     using EnumerableSet for EnumerableSet.AddressSet;
+    using SignatureValidator for EnumerableSet.AddressSet;
     bool initialized = false;
 
     EnumerableSet.AddressSet private _validators;
@@ -76,5 +83,15 @@ contract ValidatorManager is Ownable2Step {
         for (uint256 i = length; i > 0; i--) {
             _validators.remove(_validators.at(i - 1));
         }
+    }
+
+    function validateMessageWithSupermajority(
+        bytes32 ethSignedMessageHash,
+        bytes[] calldata signatures
+    ) external view {
+        _validators.validateSignaturesWithSupermajority(
+            ethSignedMessageHash,
+            signatures
+        );
     }
 }
