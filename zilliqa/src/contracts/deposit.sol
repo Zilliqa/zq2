c0f5bb0165e105be14f583a341001465aefc5e05
Yaron <yaron@zilliqa.com>
2024-08-23 20:38:50 +0800

    Add the signer address to setStake so that genesis accounts don't have a zero signer address.

--- zilliqa/src/contracts/deposit.sol
+++ zilliqa/src/contracts/deposit.sol
@@ -177,6 +177,7 @@ contract Deposit {
         bytes calldata blsPubKey,
         bytes calldata peerId,
         address rewardAddress,
+        address signerAddress,
         uint256 amount
     ) public {
         require(msg.sender == address(0));
@@ -191,6 +192,7 @@ contract Deposit {
         _stakersMap[blsPubKey].balance = amount;
         totalStake += amount;
         _stakersMap[blsPubKey].rewardAddress = rewardAddress;
+        _stakersMap[blsPubKey].signerAddress = signerAddress;
         _stakersMap[blsPubKey].peerId = peerId;
         uint256 keyIndex = _stakersMap[blsPubKey].keyIndex;
         if (keyIndex == 0) {
