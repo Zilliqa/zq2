3ec1dbc415ff81699f01205dd806e3f792117387
Yaron <yaron@zilliqa.com>
2024-08-26 16:42:32 +0800

    Fix clippy

--- zilliqa/src/crypto.rs
+++ zilliqa/src/crypto.rs
@@ -153,10 +153,6 @@ impl NodePublicKey {
         }
         Ok(())
     }
-
-    pub fn into_addr(&self) -> Address {
-        Address::from_slice(&Keccak256::digest(self.as_bytes())[12..32])
-    }
 }
 
 impl Display for NodePublicKey {
