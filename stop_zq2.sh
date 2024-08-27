54091820d51fa657a2746ceabf34b2a3e10ab522
Yaron <yaron@zilliqa.com>
2024-08-27 14:06:35 +0800

    Fix tests.

--- stop_zq2.sh
+++
@@ -1,3 +0,0 @@
-#!/opt/homebrew/bin/bash
-zq2_nodes=$(\ps -ef | grep -E 'target/[^\/]+/zilliqa --config-file ./infra/config.toml' | grep -v grep | gawk '{print $2}')
-[[ -n "${zq2_nodes}" ]] && echo "${zq2_nodes}" | xargs kill
