0a15435fce2866a7da241c531568ded8b0aca6fc
Max Conway <conway.max1@gmail.com>
2024-08-28 10:58:27 +0100

    Remove placeholder data from lists returned by DS APIs

--- zilliqa/src/api/zil.rs
+++ zilliqa/src/api/zil.rs
@@ -19,8 +19,8 @@ use super::{
     to_hex::ToHex,
     types::zil::{
         self, BlockchainInfo, DSBlock, DSBlockHeaderVerbose, DSBlockListing, DSBlockListingResult,
-        DSBlockRateResult, DSBlockVerbose, GetCurrentDSCommResult, PoWWinnerIP, SWInfo,
-        ShardingStructure, SmartContract,
+        DSBlockRateResult, DSBlockVerbose, GetCurrentDSCommResult, SWInfo, ShardingStructure,
+        SmartContract,
     },
 };
 use crate::{
@@ -495,36 +495,12 @@ fn get_example_ds_block_verbose(dsblocknum: u64, txblocknum: u64) -> DSBlockVerb
             difficulty_ds: 156,
             epoch_num: txblocknum.to_string(),
             gas_price: String::from("2000000000"),
-            members_ejected: vec![
-              "0x02572A2FCD59F8115297B399F76D7ACCFDA7E82AC53702063C3A61FB4D85E0D0C1".into(),
-              "0x029933F07FF634654C2ECB17A90EAD00CF9EE9F75395E206660CCEFB21874ECEA1".into(),
-              "0x02AAD92E5A3C9D8ECB364225719478B51026DD5C786BF7312C5C9765353BC4C98B".into()
-            ],
-            po_wwinners: vec![
-              "0x0207184EB580333132787B360CA6D93290000C9F71E0B6A02C4412E7148FB1AF81".into(),
-              "0x0285B572471A9D3BA729719ED2EEE86395D3B8F243572E9099A5E8B750F46092A7".into(),
-              "0x02C1D8C0C7884E65A22FFD76DF9ACC2EA3551133E4ADD59C2DF74F327E09F709FF".into(),
-              "0x02D728E77C8DA14E900BA8A2014A0D4B5512C6BABCCB77B83F21381437E0038F44".into(),
-              "0x0321B0E1A20F02C99394DD24B34AB4E79AE6CBF0C689C222F246431A764D6B59DB".into(),
-              "0x038A724504899CCCA068BD165AE15CE2947667225C72912039CEE4EF3992334843".into(),
-              "0x03AB477A7A895DD4E84F240A2F1FCF5F86B1A3D59B6AD3065C18CD69729D089959".into(),
-              "0x03B29C7F3F85329B0621914AB0367BA78135889FB8E4F937DDB7DAA8123AD4DF3C".into(),
-              "0x03E82B00B53ECC10073404E844841C519152E500A655EEF1D8EAD6612ABDF5B552".into()
-            ],
-            po_wwinners_ip: vec![
-                PoWWinnerIP { ip: String::from("192.0.2.0"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.1"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.2"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.3"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.4"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.5"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.6"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.7"), port: 33133 },
-                PoWWinnerIP { ip: String::from("192.0.2.8"), port: 33133 },
-            ],
+            members_ejected: vec![],
+            po_wwinners: vec![],
+            po_wwinners_ip: vec![],
             prev_hash: String::from("585373fb2c607b324afbe8f592e43b40d0091bbcef56c158e0879ced69648c8e"),
             reserved_field: String::from("0000000000000000000000000000000000000000000000000000000000000000"),
-            swinfo: SWInfo { scilla: vec![0, 0, 0, 0, 0], zilliqa: vec![0, 0, 0, 0, 0] },
+            swinfo: SWInfo { scilla: vec![], zilliqa: vec![] },
             sharding_hash: String::from("3216a33bfd4801e1907e72c7d529cef99c38d57cd281d0e9d726639fd9882d25"),
             timestamp: String::from("1606443830834512"),
             version: 2,
@@ -577,10 +553,7 @@ pub fn get_current_ds_comm(
         current_dsepoch: num_ds_blocks.to_string(),
         current_tx_epoch: num_tx_blocks.to_string(),
         num_of_dsguard: 420,
-        dscomm: vec![
-            "0x020035B739426374C5327A1224B986005297102E01C29656B8B086BF4B352C6CA9".into(),
-            "0x0200834D709AD621785A90673F6011BC36ECF4CB13475237EAA2D4DEDAE7E9E554".into(),
-        ],
+        dscomm: vec![],
     })
 }
 
