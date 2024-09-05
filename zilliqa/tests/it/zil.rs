bc94ffb8d34d61d4e38c1243bc25b04e60717a21
Max Conway <conway.max1@gmail.com>
2024-09-05 14:20:36 +0100

    Enhanced test for get_tx_rate

--- zilliqa/tests/it/zil.rs
+++ zilliqa/tests/it/zil.rs
@@ -727,4 +727,20 @@ async fn get_tx_rate(mut network: Network) {
         "Transaction block rate should be non-negative"
     );
     assert!(tx_rate >= 0.0, "Transaction rate should be non-negative");
+
+    network.run_until_block(&wallet, 2.into(), 50).await;
+
+    let response: Value = wallet
+        .provider()
+        .request("GetTransactionRate", [""])
+        .await
+        .expect("Failed to call GetTxRate API");
+
+    let tx_rate: f64 = serde_json::from_value(response).expect("Failed to deserialize response");
+
+    assert!(
+        tx_rate >= 0.0,
+        "Transaction block rate should be non-negative"
+    );
+    assert!(tx_rate >= 0.0, "Transaction rate should be non-negative");
 }
