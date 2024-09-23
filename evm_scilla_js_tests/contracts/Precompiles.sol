0612b7bee9089207a094d1383492a669cc8865e7
saeed-zil <saeed-zil@users.noreply.github.com>
2024-09-23 12:19:04 +0000

    Apply formatting changes to solidity files

--- evm_scilla_js_tests/contracts/Precompiles.sol
+++ evm_scilla_js_tests/contracts/Precompiles.sol
@@ -2,59 +2,58 @@
 pragma solidity ^0.8.9;
 
 contract Precompiles {
-
   bytes public idStored;
   uint256 public modExpResult;
 
   constructor() {}
 
-  function testRecovery(bytes32 documentHash, uint8 v, bytes32 r, bytes32 s) pure public returns (address) {
-      bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-      bytes32 prefixedProof = keccak256(abi.encodePacked(prefix, documentHash));
-      address recovered = ecrecover(prefixedProof, v, r, s);
-      return recovered;
+  function testRecovery(bytes32 documentHash, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
+    bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+    bytes32 prefixedProof = keccak256(abi.encodePacked(prefix, documentHash));
+    address recovered = ecrecover(prefixedProof, v, r, s);
+    return recovered;
   }
 
   function testIdentity(bytes memory data) public {
-      bytes memory result = new bytes(data.length);
-      assembly {
-        let len := mload(data)
-          if iszero(call(gas(), 0x04, 0, add(data, 0x20), len, add(result,0x20), len)) {
-            invalid()
-          }
-        }
-      idStored = result;
+    bytes memory result = new bytes(data.length);
+    assembly {
+      let len := mload(data)
+      if iszero(call(gas(), 0x04, 0, add(data, 0x20), len, add(result, 0x20), len)) {
+        invalid()
+      }
+    }
+    idStored = result;
   }
 
-  function testSHA256(string memory word) pure public returns (bytes32) {
-        bytes32 hash = sha256(bytes (word));
-        return hash;        
+  function testSHA256(string memory word) public pure returns (bytes32) {
+    bytes32 hash = sha256(bytes(word));
+    return hash;
   }
 
-  function testRipemd160(string memory word) pure public returns (bytes20) {
-        bytes20 hash = ripemd160(bytes (word));
-        return hash;        
+  function testRipemd160(string memory word) public pure returns (bytes20) {
+    bytes20 hash = ripemd160(bytes(word));
+    return hash;
   }
-  
-  function testModexp( uint _base, uint _exp, uint _modulus) public {
-        modExpResult = _modExp(_base, _exp, _modulus);
+
+  function testModexp(uint _base, uint _exp, uint _modulus) public {
+    modExpResult = _modExp(_base, _exp, _modulus);
   }
 
   function _modExp(uint256 base, uint256 exponent, uint256 modulus) internal returns (uint256 result) {
-        assembly {
-            let pointer := mload(0x40)
-            mstore(pointer, 0x20)
-            mstore(add(pointer, 0x20), 0x20)
-            mstore(add(pointer, 0x40), 0x20)
-            mstore(add(pointer, 0x60), base)
-            mstore(add(pointer, 0x80), exponent)
-            mstore(add(pointer, 0xa0), modulus)
-            let value := mload(0xc0)
-            if iszero(call(gas(), 0x05, 0, pointer, 0xc0, value, 0x20)) {
-                revert(0, 0)
-            }
-            result := mload(value)
-        }
+    assembly {
+      let pointer := mload(0x40)
+      mstore(pointer, 0x20)
+      mstore(add(pointer, 0x20), 0x20)
+      mstore(add(pointer, 0x40), 0x20)
+      mstore(add(pointer, 0x60), base)
+      mstore(add(pointer, 0x80), exponent)
+      mstore(add(pointer, 0xa0), modulus)
+      let value := mload(0xc0)
+      if iszero(call(gas(), 0x05, 0, pointer, 0xc0, value, 0x20)) {
+        revert(0, 0)
+      }
+      result := mload(value)
+    }
   }
 
   function testEcAdd(uint256 a_x, uint256 a_y, uint256 b_x, uint256 b_y) public returns (uint256[2] memory p) {
@@ -98,7 +97,13 @@ contract Precompiles {
     return pairs[0];
   }
 
-  function testBlake2(uint32 rounds, bytes32[2] memory h, bytes32[4] memory m, bytes8[2] memory t, bool f) public returns (bytes32[2] memory) {
+  function testBlake2(
+    uint32 rounds,
+    bytes32[2] memory h,
+    bytes32[4] memory m,
+    bytes8[2] memory t,
+    bool f
+  ) public returns (bytes32[2] memory) {
     bytes32[2] memory output;
 
     bytes memory args = abi.encodePacked(rounds, h[0], h[1], m[0], m[1], m[2], m[3], t[0], t[1], f);
--- zilliqa/tests/it/contracts/SetGetContractValue.sol
+++ zilliqa/tests/it/contracts/SetGetContractValue.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.20;
 
 contract SetGetContractValue {
-  uint256 private value = 99;
+    uint256 private value = 99;
 
-  constructor() {}
+    constructor() {}
 
-  function getUint256() public view returns (uint256) {
-    return value;
-  }
+    function getUint256() public view returns (uint256) {
+        return value;
+    }
 
-  function setUint256(uint256 _value) public {
-    value = _value;
-  }
+    function setUint256(uint256 _value) public {
+        value = _value;
+    }
 }
