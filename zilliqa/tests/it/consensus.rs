ec5acb9e6bc4cbdd3d92fdaeaab322cd220663fd
Richard Watts <richard@zilliqa.com>
2024-09-30 16:46:02 +0100

    (fix) Fix cross shard link test by giving network.run_until_receipt() a bit more time to work

--- zilliqa/tests/it/consensus.rs
+++ zilliqa/tests/it/consensus.rs
@@ -245,7 +245,7 @@ async fn create_shard(
 
     let tx = wallet.send_transaction(tx_request, None).await.unwrap();
     let hash = tx.tx_hash();
-    network.run_until_receipt(wallet, hash, 130).await;
+    network.run_until_receipt(wallet, hash, 200).await;
 
     let included_block = wallet.get_block_number().await.unwrap();
 
