1e0baa422be74e2ff68aed3dd3126524ec7f3f53
saeed-zil <saeed@zilliqa.com>
2024-10-10 17:32:56 +0330

    remove extra checks

--- evm_scilla_js_tests/test/BlockProperties.ts
+++ evm_scilla_js_tests/test/BlockProperties.ts
@@ -7,14 +7,14 @@ const ZERO = "0x0000000000000000000000000000000000000000000000000000000000000000
 
 describe("Block Properties", function () {
   let contract: Contract;
-  let fixedBlockNumber: number;
+  let toBeTestedBlockNumber: number;
 
   before(async function () {
     contract = await hre.deployContract("BlockAndTransactionProperties");
   });
 
   beforeEach(async function () {
-    fixedBlockNumber = await ethers.provider.getBlockNumber();
+    toBeTestedBlockNumber = await ethers.provider.getBlockNumber();
   });
 
   it("Should be deployed successfully @block-1", async function () {
@@ -22,26 +22,26 @@ describe("Block Properties", function () {
   });
 
   it("should return the correct block number", async function () {
-    const contractBlockNumber = await contract.getBlockNumber({blockTag: fixedBlockNumber});
-    expect(contractBlockNumber).to.equal(fixedBlockNumber);
+    const contractBlockNumber = await contract.getBlockNumber({blockTag: toBeTestedBlockNumber});
+    expect(contractBlockNumber).to.equal(toBeTestedBlockNumber);
   });
 
   it("should return the correct block timestamp", async function () {
-    const contractBlockTimestamp = await contract.getTimestamp({blockTag: fixedBlockNumber});
-    const currentBlock = await ethers.provider.getBlock(fixedBlockNumber);
+    const contractBlockTimestamp = await contract.getTimestamp({blockTag: toBeTestedBlockNumber});
+    const currentBlock = await ethers.provider.getBlock(toBeTestedBlockNumber);
 
     expect(contractBlockTimestamp).to.equal(currentBlock.timestamp);
   });
 
   xit("should return the correct base fee", async function () {
-    const contractBaseFee = await contract.getBaseFee({blockTag: fixedBlockNumber});
-    const currentBlock = await ethers.provider.getBlock(fixedBlockNumber);
+    const contractBaseFee = await contract.getBaseFee({blockTag: toBeTestedBlockNumber});
+    const currentBlock = await ethers.provider.getBlock(toBeTestedBlockNumber);
 
     expect(contractBaseFee).to.equal(currentBlock.baseFeePerGas);
   });
 
   it("should return the correct chain id", async function () {
-    const contractChainId = await contract.getChainId({blockTag: fixedBlockNumber});
+    const contractChainId = await contract.getChainId({blockTag: toBeTestedBlockNumber});
     const network = await ethers.provider.getNetwork();
 
     expect(contractChainId).to.equal(network.chainId);
@@ -55,27 +55,28 @@ describe("Block Properties", function () {
   });
 
   it("should return the correct gas limit", async function () {
-    const contractGasLimit = await contract.getGasLimit({blockTag: fixedBlockNumber});
-    const currentBlock = await ethers.provider.getBlock(fixedBlockNumber);
+    const contractGasLimit = await contract.getGasLimit({blockTag: toBeTestedBlockNumber});
+    const currentBlock = await ethers.provider.getBlock(toBeTestedBlockNumber);
 
     expect(contractGasLimit).to.equal(currentBlock.gasLimit);
   });
 
   it("should emit blockhash correctly", async function () {
-    const tx = await contract.emitBlockHash(fixedBlockNumber);
-    const actualBlockHash = (await ethers.provider.getBlock(fixedBlockNumber)).hash;
+    const tx = await contract.emitBlockHash(toBeTestedBlockNumber);
+    const actualBlockHash = (await ethers.provider.getBlock(toBeTestedBlockNumber)).hash;
 
     await expect(tx).to.emit(contract, "BlockHash").withArgs(actualBlockHash);
   });
 
   it("should emit zero blockhash for current block that's not finalized yet", async function () {
-    const tx = await contract.emitBlockHash(fixedBlockNumber + 1);
+    const tx = await contract.emitBlockHash(toBeTestedBlockNumber + 1);
     await expect(tx).to.emit(contract, "BlockHash").withArgs(ZERO);
   });
 
   it("should emit zero blockhash for a block number more than 256 blocks in the past", async function () {
-    if (fixedBlockNumber > 256) {
-      const tx = await contract.emitBlockHash(fixedBlockNumber - 256);
+    const blockNumber = await ethers.provider.getBlockNumber();
+    if (blockNumber > 256) {
+      const tx = await contract.emitBlockHash(blockNumber - 256);
       await expect(tx).to.emit(contract, "BlockHash").withArgs(ZERO);
     } else {
       this.skip(); // Not possible to test if we don't have 256 or more blocks
@@ -83,7 +84,7 @@ describe("Block Properties", function () {
   });
 
   it("should emit zero blockhash for a future block number", async function () {
-    const tx = await contract.emitBlockHash(fixedBlockNumber + 100);
+    const tx = await contract.emitBlockHash(toBeTestedBlockNumber + 100);
     await expect(tx).to.emit(contract, "BlockHash").withArgs(ZERO);
   });
 });
