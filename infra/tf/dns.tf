a67cc94373582f36bbe77b2443d6cfb3e8cf3633
Francesco Medas <francesco@zilliqa.com>
2024-10-17 18:31:27 +0400

    fix

--- infra/tf/dns.tf
+++ infra/tf/dns.tf
@@ -19,9 +19,9 @@ resource "google_dns_record_set" "this" {
 
   project      = var.dns_zone_project_id
   managed_zone = replace(var.nodes_dns_zone_name, ".", "-")
-  name         = "${each.value.name}.${var.network_name}.${var.nodes_dns_zone_name}."
+  name         = "${each.value[idx].name}.${var.network_name}.${var.nodes_dns_zone_name}."
   type         = "A"
   ttl          = "60"
 
-  rrdatas = [each.value.external_ip]
+  rrdatas = [each.value[idx].external_ip]
 }
