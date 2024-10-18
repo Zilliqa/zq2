46cb5384c7eedf817d6076c546d2fef7ad375c6b
Francesco Medas <francesco@zilliqa.com>
2024-10-18 14:33:41 +0400

    fix

--- infra/tf/modules/node2/main.tf
+++ infra/tf/modules/node2/main.tf
@@ -2,7 +2,13 @@
 # changes and any instance groups containing them are updated.
 
 resource "random_bytes" "generate_node_key" {
-  count  = var.generate_node_key ? length(var.config.nodes) * var.config.nodes[count.index].count : 0
+  # count  = var.generate_node_key ? length(var.config.nodes) * var.config.nodes[count.index].count : 0
+
+  for_each = { for idx, node in var.config.nodes : idx => node }
+
+  count = each.value.count
+
+
   length = 32
 }
 
