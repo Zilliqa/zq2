43c160a526f1c3155ad3ae69bdc14febe690eecd
Francesco Medas <francesco@zilliqa.com>
2024-10-18 17:56:48 +0400

    fix

--- infra/tf/modules/node2/datasources.tf
+++ infra/tf/modules/node2/datasources.tf
@@ -10,8 +10,12 @@ data "google_compute_zones" "available" {
   region = lookup(var.config.nodes[count.index], "region", null)
 }
 
-data "google_compute_subnetwork" "default" {
+# data "google_compute_subnetwork" "default" {
+#   project = data.google_project.current.project_id
+#   # region  = var.region
+#   # name    = var.vpc_main_subnet_name
+# }
+
+data "google_compute_subnetworks" "default" {
   project = data.google_project.current.project_id
-  # region  = var.region
-  # name    = var.vpc_main_subnet_name
 }
