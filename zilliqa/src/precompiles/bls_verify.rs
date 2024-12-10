f522c36efe74ab58044b325285faa64806d27325
Tomos Wootton <tomos@zilliqa.com>
2024-12-10 17:16:16 +0000

    feat: New precompile bls_verify

--- zilliqa/src/precompiles/bls_verify.rs
+++ zilliqa/src/precompiles/bls_verify.rs
@@ -11,68 +11,59 @@ use revm::{
 
 use crate::exec::PendingState;
 
-pub struct PopVerify;
+pub struct BlsVerify;
 
-/// A custom Proof of Possession in which the bls public key, an evm address and a chain_id are signed over
-// keep in-sync with zilliqa/src/contracts/deposit.sol
-impl PopVerify {
-    const POP_VERIFY_GAS_PRICE: u64 = 1_000_000u64; // FIXME: Gas Price?
-    fn pop_verify(
+// keep in-sync with zilliqa/src/contracts/deposit_v3.sol
+impl BlsVerify {
+    const BLS_VERIFY_GAS_PRICE: u64 = 1_000_000u64; // FIXME: Gas Price?
+    fn bls_verify(
         input: &[u8],
         gas_limit: u64,
         _context: &mut InnerEvmContext<PendingState>,
     ) -> PrecompileResult {
-        if gas_limit < Self::POP_VERIFY_GAS_PRICE {
+        if gas_limit < Self::BLS_VERIFY_GAS_PRICE {
             return Err(PrecompileErrors::Error(PrecompileError::OutOfGas));
         }
 
-        let Ok(decoded) = decode(&[ParamType::Bytes, ParamType::Bytes], input) else {
+        let Ok(decoded) = decode(
+            &[ParamType::Bytes, ParamType::Bytes, ParamType::Bytes],
+            input,
+        ) else {
             return Err(PrecompileError::Other("ABI input decoding error!".into()).into());
         };
-        if decoded.len() != 2 {
-            // expected 2 arguments
+        if decoded.len() != 3 {
+            // expected 3 arguments
             return Err(PrecompileError::Other("ABI inputs missing".into()).into());
         };
 
-        let Ok(pop) = <blsful::Bls12381G2Impl as blsful::Pairing>::Signature::try_from(
-            decoded[0].to_owned().into_bytes().unwrap(),
+        let message = decoded[0].to_owned().into_bytes().unwrap();
+
+        let Ok(signature) = <blsful::Bls12381G2Impl as blsful::Pairing>::Signature::try_from(
+            decoded[1].to_owned().into_bytes().unwrap(),
         ) else {
             return Err(PrecompileError::Other("ABI signature invalid".into()).into());
         };
 
         let Ok(pk) = blsful::PublicKey::<Bls12381G2Impl>::try_from(
-            decoded[1].to_owned().into_bytes().unwrap(),
+            decoded[2].to_owned().into_bytes().unwrap(),
         ) else {
             return Err(PrecompileError::Other("ABI pubkey invalid".into()).into());
         };
 
-        // message which pop signs over
-        let mut pop_message = [0u8; 98];
-        pop_message[..48].copy_from_slice(&pk.0.to_compressed());
-        pop_message[48..56].copy_from_slice(&_context.env.cfg.chain_id.to_le_bytes());
-        pop_message[56..].copy_from_slice(
-            &_context
-                .env
-                .tx
-                .caller
-                .to_checksum_buffer(Some(_context.env.cfg.chain_id))
-                .into_inner(),
-        );
-
-        let result = blsful::Signature::Basic(pop)
-            .verify(&pk, pop_message)
+        let result = blsful::Signature::Basic(signature)
+            .verify(&pk, message)
             .is_ok();
 
         // FIXME: Gas?
         let output = encode(&[Token::Bool(result)]);
         Ok(PrecompileOutput::new(
-            Self::POP_VERIFY_GAS_PRICE,
+            Self::BLS_VERIFY_GAS_PRICE,
             output.into(),
         ))
     }
 }
 
-impl ContextStatefulPrecompile<PendingState> for PopVerify {
+impl ContextStatefulPrecompile<PendingState> for BlsVerify {
     fn call(
         &self,
         input: &Bytes,
@@ -87,8 +78,11 @@ impl ContextStatefulPrecompile<PendingState> for PopVerify {
         }
 
         let dispatch_table: [([u8; 4], _); 1] = [(
-            short_signature("popVerify", &[ParamType::Bytes, ParamType::Bytes]),
-            Self::pop_verify,
+            short_signature(
+                "blsVerify",
+                &[ParamType::Bytes, ParamType::Bytes, ParamType::Bytes],
+            ),
+            Self::bls_verify,
         )];
 
         let Some(handler) = dispatch_table
