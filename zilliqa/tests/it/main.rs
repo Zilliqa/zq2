658417eacad61565f002c07607f8a309b408a92e
Tomos Wootton <tomos@zilliqa.com>
2024-12-23 09:33:11 +0000

    fix: rebase corrections

--- zilliqa/tests/it/main.rs
+++ zilliqa/tests/it/main.rs
@@ -67,8 +67,7 @@ use zilliqa::{
     cfg::{
         allowed_timestamp_skew_default, block_request_batch_size_default,
         block_request_limit_default, eth_chain_id_default, failed_request_sleep_duration_default,
-        max_blocks_in_flight_default, max_rpc_response_size_default,
-        minimum_time_left_for_empty_block_default, scilla_address_default,
+        max_blocks_in_flight_default, max_rpc_response_size_default, scilla_address_default,
         scilla_ext_libs_path_default, scilla_stdlib_dir_default, state_cache_size_default,
         state_rpc_limit_default, total_native_token_supply_default, Amount, ApiServer, Checkpoint,
         ConsensusConfig, ContractUpgradesBlockHeights, Forks, GenesisDeposit, NodeConfig,
