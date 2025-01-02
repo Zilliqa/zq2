3b5910bce75e877e536bf0f1657c2c3e02c2b455
Tomos Wootton <tomos@zilliqa.com>
2025-01-02 11:45:52 +0000

    fix: Change namings to make code purpose clearer

--- z2/src/setup.rs
+++ z2/src/setup.rs
@@ -24,7 +24,7 @@ use zilliqa::{
 use zilliqa::{
     cfg::{
         self, allowed_timestamp_skew_default, block_request_batch_size_default,
-        block_request_limit_default, consensus_timeout_default, empty_block_timeout_default,
+        block_request_limit_default, block_time_default, consensus_timeout_default,
         eth_chain_id_default, failed_request_sleep_duration_default, local_address_default,
         max_blocks_in_flight_default, scilla_address_default, scilla_ext_libs_path_default,
         scilla_stdlib_dir_default, state_rpc_limit_default, total_native_token_supply_default,
@@ -527,7 +527,7 @@ impl Setup {
                     eth_block_gas_limit: EvmGas(84000000),
                     gas_price: 4_761_904_800_000u128.into(),
                     minimum_stake: 10_000_000_000_000_000_000_000_000u128.into(),
-                    empty_block_timeout: empty_block_timeout_default(),
+                    block_time: block_time_default(),
                     genesis_accounts: Vec::new(),
                     is_main: true,
                     blocks_per_hour: 3600,
