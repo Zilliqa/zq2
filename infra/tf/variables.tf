877fafb78424dc3fd51ae956f0cb5a2cc30a4617
Pablo Suarez <pablosynth@gmail.com>
2025-02-02 16:11:50 +0400

    feat: DEVOPS-1833 jmeter test pipeline

--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -375,3 +375,9 @@ variable "persistence_bucket_versioning" {
   default     = true
   nullable    = false
 }
+
+variable "testing_service_account" {
+  description = "Email of the testing service account"
+  type        = string
+  default     = "sa-gha-testing-001@prj-p-devops-services-tvwmrf63.iam.gserviceaccount.com"
+}
