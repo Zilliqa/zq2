996f9ba1a79b82c17cb5e412c43acb710e23180c
Tomos Wootton <tomos@zilliqa.com>
2025-02-24 16:22:12 +0000

    fix: do to panic inside leader_at_block()

--- zilliqa/src/consensus.rs
+++ zilliqa/src/consensus.rs
@@ -2245,7 +2245,13 @@ impl Consensus {
         }
 
         // Derive the proposer from the block's view
-        let proposer = self.leader_at_block(&parent, block.view()).unwrap();
+        let Some(proposer) = self.leader_at_block(&parent, block.view()) else {
+            return Err(anyhow!(
+                "Failed to find leader. Block number {}, Parent number {}",
+                block.number(),
+                parent.number(),
+            ));
+        };
 
         // Verify the proposer's signature on the block
         let verified = proposer
