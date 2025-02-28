378e9d5812fa2d6e8f361ef09c2ee5c576951e6d
Tomos Wootton <tomos@zilliqa.com>
2025-02-28 12:12:27 +0000

    chore: update protomainnet scilla json ordering hardfork height

--- z2/src/chain.rs
+++ z2/src/chain.rs
@@ -211,8 +211,9 @@ impl Chain {
             Chain::Zq2ProtoMainnet => Some(vec![
                 // estimated: 2024-12-20T23:33:12Z
                 json!({ "at_height": 5342400, "failed_scilla_call_from_gas_exempt_caller_causes_revert": true, "call_mode_1_sets_caller_to_parent_caller": true }),
+                json!({ "at_height": 7685881, "scilla_json_preserve_order": true }),
                 // estimated: 2025-02-12T13:25:00Z
-                json!({ "at_height": 7966800, "scilla_messages_can_call_evm_contracts": true, "scilla_contract_creation_increments_account_balance": true, "scilla_json_preserve_order": true }),
+                json!({ "at_height": 7966800, "scilla_messages_can_call_evm_contracts": true, "scilla_contract_creation_increments_account_balance": true }),
             ]),
             _ => None,
         }
