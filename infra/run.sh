dedc7c1ddd2705fe2c8294bfaffc13adc93a1769
Tomos Wootton <tomos@zilliqa.com>
2025-03-10 15:26:22 +0000

    feat: allow specification of scilla server port in start_up script

--- infra/run.sh
+++ infra/run.sh
@@ -3,7 +3,9 @@
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 
 # Start Scilla server
-/scilla/0/bin/scilla-server-http &
+/scilla/0/bin/scilla-server-http --port=$1 &
+
+shift
 
 # Start Zilliqa
 /zilliqa $@
