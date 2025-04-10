7747029ebae9f316c930fb5d1323e5a53570851d
Francesco Medas <francesco@zilliqa.com>
2025-04-10 18:58:45 +0400

    feat: DEVOPS-1952 add internal node name

--- infra/tf/outputs.tf
+++ infra/tf/outputs.tf
@@ -34,3 +34,10 @@ output "persistence_external_ip" {
   description = "The provisioned bootstrap external IPs"
   value       = module.persistences.external_ip
 }
+
+output "private_api_external_ip" {
+  description = "The provisioned private APIs external IPs"
+  value       = merge(flatten([
+    for private_api in module.private_apis : private_api.external_ip
+  ])...)
+}
