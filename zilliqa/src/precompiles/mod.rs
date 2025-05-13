5de065a56ad3bdb8114ea5874de71064be07001e
Shawn <shawn@zilliqa.com>
2025-05-13 12:54:10 +0800

    remove ERC20 precompile and test.

--- zilliqa/src/precompiles/mod.rs
+++ zilliqa/src/precompiles/mod.rs
@@ -1,5 +1,4 @@
 mod bls_verify;
-mod erc20;
 mod pop_verify;
 mod scilla;
 
@@ -7,7 +6,6 @@ use std::sync::Arc;
 
 use alloy::primitives::Address;
 use bls_verify::BlsVerify;
-use erc20::ERC20Precompile;
 use pop_verify::PopVerify;
 use revm::ContextPrecompile;
 use scilla::ScillaRead;
@@ -17,10 +15,6 @@ use crate::exec::PendingState;
 
 pub fn get_custom_precompiles() -> Vec<(Address, ContextPrecompile<PendingState>)> {
     vec![
-        (
-            Address::from(*b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0ZIL"),
-            ContextPrecompile::ContextStateful(Arc::new(ERC20Precompile)),
-        ),
         (
             Address::from(*b"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0ZIL\x80"),
             ContextPrecompile::ContextStateful(Arc::new(PopVerify)),
