1cb79e200a1a8835b55b0a321c77a0f9e01f687f
Max Conway <conway.max1@gmail.com>
2025-05-20 14:30:54 +0100

    Fix admin_votesReceived bug (#2782)
    Problem was that BTreeMap doesn't like to serialize with non-numeric keys
    So I changed to vecs
    
    Closes #2780

--- zilliqa/src/api/types/admin.rs
+++ zilliqa/src/api/types/admin.rs
@@ -1,10 +1,8 @@
-use std::collections::BTreeMap;
-
 use serde::Serialize;
 
 #[derive(Clone, Debug, Serialize)]
 pub struct VotesReceivedReturnee {
-    pub votes: BTreeMap<crate::crypto::Hash, crate::consensus::BlockVotes>,
-    pub buffered_votes: BTreeMap<crate::crypto::Hash, Vec<crate::message::Vote>>,
-    pub new_views: BTreeMap<u64, crate::consensus::NewViewVote>,
+    pub votes: Vec<(crate::crypto::Hash, crate::consensus::BlockVotes)>,
+    pub buffered_votes: Vec<(crate::crypto::Hash, Vec<crate::message::Vote>)>,
+    pub new_views: Vec<(u64, crate::consensus::NewViewVote)>,
 }
