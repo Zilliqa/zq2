8a43fdbc44f2d0fcaa67e56b291f5c83aa2259f8
Shawn <shawn@zilliqa.com>
2025-07-01 14:12:54 +0800

    better description of attempts * timeouts.

--- zilliqa/src/scilla.rs
+++ zilliqa/src/scilla.rs
@@ -220,7 +220,9 @@ pub struct Scilla {
 }
 
 impl Scilla {
-    const MAX_ATTEMPTS: u8 = 2;
+    const MAX_ATTEMPTS: u8 = 3; // effective time is up to MAX_ATTEMPTS * REQ_TIMEOUT.
+    const REQ_TIMEOUT: Duration = Duration::from_secs(120); // effective time should be kept below gossip/request timeouts.
+
     /// Create a new Scilla interpreter. This involves spawning two threads:
     /// 1. The client thread, responsible for communicating with the server.
     /// 2. The state IPC thread, responsible for serving state requests from the running Scilla server.
@@ -250,7 +252,7 @@ impl Scilla {
                 .build()
                 .unwrap();
             let client = HttpClientBuilder::default()
-                .request_timeout(Duration::from_secs(120))
+                .request_timeout(Self::REQ_TIMEOUT)
                 .build(format!("{address}/run"))
                 .unwrap();
 
