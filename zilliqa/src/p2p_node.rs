c3a617fe3b967ee5cdae149cfb372c2c3c60e488
Shawn <shawn@zilliqa.com>
2025-07-15 12:11:10 +0800

    increase proposal size 1MB -> 2MB, to allow 4K ZIL transfers per block.

--- zilliqa/src/p2p_node.rs
+++ zilliqa/src/p2p_node.rs
@@ -142,8 +142,8 @@ impl P2pNode {
                     gossipsub: gossipsub::Behaviour::new(
                         MessageAuthenticity::Signed(key_pair.clone()),
                         gossipsub::ConfigBuilder::default()
-                            // 1MB is sufficient to accommodate proposal with 4000 simple transfers (block gas limit)
-                            .max_transmit_size(1024 * 1024)
+                            // 2MB is sufficient to accommodate proposal with 4000 simple transfers (block gas limit)
+                            .max_transmit_size(1024 * 1024 * 2)
                             // Increase the duplicate cache time to reduce the likelihood of delayed messages being
                             // mistakenly re-propagated and flooding the network.
                             .duplicate_cache_time(Duration::from_secs(3600))
