bc0311f7161c5351ffb462ab3bf5fd7e6fde46a5
Bartosz Zawistowski <39065214+bzawisto@users.noreply.github.com>
2025-07-18 09:35:36 +0200

    Make an exception for scilla precompile in view/estimate calls (#3042)
    * Make an exception for scilla precompile in view/estimate calls
    
    * Fix
    
    ---------
    
    Co-authored-by: DrZoltanFazekas <55164848+DrZoltanFazekas@users.noreply.github.com>

--- zilliqa/src/node.rs
+++ zilliqa/src/node.rs
@@ -902,7 +902,7 @@ impl Node {
             return Err(anyhow!("State required to execute request does not exist"));
         }
 
-        state.call_contract(from_addr, to_addr, data, amount, block.header, true)
+        state.call_contract(from_addr, to_addr, data, amount, block.header)
     }
 
     pub fn get_proposer_reward_address(&self, header: BlockHeader) -> Result<Option<Address>> {
@@ -959,7 +959,6 @@ impl Node {
             gas,
             gas_price,
             value,
-            true,
         )
     }
 
