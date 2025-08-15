7a2286b2e5848b9f68b4c1e0939d9cf91c312a97
Shawn <shawn@zilliqa.com>
2025-08-15 11:14:33 +0800

    added primitive rocksdb implementation to TrieStorage. added lazy-migration from SQLite -> RocksDB. added clang to docker build. moved rocks-db creation to DB::new() constructor. use single-threaded rocksdb by default added bin/revert-state.rs tool. added TrieStorage::migrate_state() to help active migration. fix CI.yaml. added LruCache to RocksDB. added gradual migration() capability - during timeout(). added config setting active_state_migration. moved init_rocksdb() to after a block has been populated. count records to migrate; rename state_trie when done.

---
+++ zilliqa/src/bin/revert-state.rs
@@ -0,0 +1,31 @@
+use std::{path::PathBuf, sync::Arc};
+
+use anyhow::Result;
+use clap::Parser;
+use zilliqa::db::Db;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input directory e.g. "./data"
+    #[arg(long)]
+    data_dir: String,
+
+    /// Shard ID
+    #[arg(long)]
+    id: u64,
+}
+
+fn main() -> Result<()> {
+    println!("ZILLIQA 2.0 Restore State");
+    let args = Args::parse();
+
+    let data_dir = PathBuf::from(args.data_dir.clone());
+    let db = Arc::new(Db::new::<PathBuf>(Some(data_dir), args.id, 0, None, true)?);
+
+    let trie_storage = Arc::new(db.state_trie()?);
+
+    trie_storage.revert_state().unwrap();
+
+    Ok(())
+}
