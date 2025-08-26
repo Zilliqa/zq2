d62abc2a582fddc443fd201cdd62864adf6d46da
Shawn <shawn@zilliqa.com>
2025-08-26 14:44:16 +0800

    added convert-ckpt.rs tool to convert checkpoint files.

--- zilliqa/src/checkpoint.rs
+++ zilliqa/src/checkpoint.rs
@@ -1,10 +1,11 @@
 use std::{
     fs::File,
     io::{BufReader, Read},
+    path::Path,
     sync::Arc,
 };
 
-use anyhow::{Result, anyhow};
+use anyhow::{Result, anyhow, ensure};
 use eth_trie::{DB, EthTrie, MemoryDB, Trie};
 use lz4::Decoder;
 
@@ -163,4 +164,250 @@ pub fn get_checkpoint_block(
     }
 
     Ok(Some((block, transactions, parent)))
+}
+
+const BIN_CONFIG: bincode::config::Configuration = bincode::config::standard();
+const CKPT_VERSION: &str = "ZILCHKPT/2.0";
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct Checkpoint {
+    pub account_count: u64,
+    pub record_count: u64,
+    pub chain_id: u64,
+    pub block_hash: Hash,
+}
+
+pub fn load_ckpt(
+    path: &Path,
+    trie_storage: Arc<TrieStorage>,
+    chain_id: u64,
+    block_hash: Hash,
+) -> Result<()> {
+    ensure!(
+        path.extension().unwrap_or_default() == "ckpt",
+        "Checkpoint {} is not .ckpt file",
+        path.display()
+    );
+
+    let mut zipreader = zip::ZipArchive::new(std::fs::File::open(path)?)?;
+
+    // Currently checks that the version matches exactly.
+    // This check also ensures that the file is a ZIP file by reading the EOCD.
+    // In the future, we may handle different versions separately,
+    //
+    // - ZILCHKPT/2.0 : Checkpoint 2.0
+    ensure!(
+        zipreader.comment() == CKPT_VERSION.as_bytes(),
+        "Invalid checkpoint version",
+    );
+
+    let meta = {
+        let mut file = zipreader.by_name("metadata.json")?;
+        let meta: Checkpoint = serde_json::from_reader(&mut file)?;
+        ensure!(
+            meta.chain_id == chain_id,
+            "Chain ID {} mismatch",
+            meta.chain_id
+        );
+        ensure!(
+            meta.block_hash == block_hash,
+            "Bock hash {} mistmatch",
+            meta.block_hash
+        );
+        meta
+    };
+    let block = {
+        let mut file = zipreader.by_name("block.bin")?;
+        let block: crate::message::Block =
+            bincode::serde::decode_from_std_read(&mut file, BIN_CONFIG)?;
+        ensure!(
+            block.verify_hash().is_ok(),
+            "Block hash {} invalid",
+            block.hash()
+        );
+        block
+    };
+    let parent = {
+        let mut file = zipreader.by_name("parent.bin")?;
+        let parent: crate::message::Block =
+            bincode::serde::decode_from_std_read(&mut file, BIN_CONFIG)?;
+        ensure!(
+            parent.verify_hash().is_ok(),
+            "Parent hash {} invalid",
+            parent.hash()
+        );
+        parent
+    };
+    ensure!(block.parent_hash() == parent.hash(), "Parent hash mismatch");
+
+    // Verify transactions list
+    let _transactions = {
+        let mut file = zipreader.by_name("transactions.bin")?;
+        let transactions: Vec<SignedTransaction> =
+            bincode::serde::decode_from_std_read(&mut file, BIN_CONFIG)?;
+        let mut transactions_trie = EthTrie::new(Arc::new(MemoryDB::new(true)));
+        for tx in &transactions {
+            let hash = tx.calculate_hash();
+            transactions_trie.insert(hash.as_bytes(), hash.as_bytes())?;
+        }
+        let transactions_root_hash = Hash(transactions_trie.root_hash()?.into());
+        ensure!(
+            block.header.transactions_root_hash == transactions_root_hash,
+            "Transactions root hash {} mismatch",
+            transactions_root_hash
+        );
+        transactions
+    };
+
+    // reconstruct the state trie from accounts/storage leaf nodes
+    let (account_count, record_count) = {
+        let mut account_storage = EthTrie::new(trie_storage.clone());
+
+        let mut account_count = 0;
+        let mut record_count = 0;
+
+        let mut reader = zipreader.by_name("state_trie.bin")?;
+        while let Ok(account_key) =
+            bincode::decode_from_std_read::<Vec<u8>, _, _>(&mut reader, BIN_CONFIG)
+        {
+            let account_val: Vec<u8> = bincode::decode_from_std_read(&mut reader, BIN_CONFIG)?;
+            account_storage.insert(account_key.as_slice(), account_val.as_slice())?;
+
+            // load the storage trie to memory
+            let mem_storage = Arc::new(MemoryDB::new(true));
+            let mut account_trie = EthTrie::new(mem_storage.clone());
+            let count: usize = bincode::serde::decode_from_std_read(&mut reader, BIN_CONFIG)?;
+            for _ in 0..count {
+                let key: Vec<u8> = bincode::decode_from_std_read(&mut reader, BIN_CONFIG)?;
+                let val: Vec<u8> = bincode::decode_from_std_read(&mut reader, BIN_CONFIG)?;
+                account_trie.insert(key.as_slice(), val.as_slice())?;
+                record_count += 1;
+            }
+
+            // compute the root trie for this account
+            let root_hash = account_trie.root_hash()?;
+            let account_root = bincode::serde::decode_from_slice::<Account, _>(
+                account_val.as_slice(),
+                bincode::config::legacy(), // for backwards compatibility with existing data
+            )?
+            .0
+            .storage_root;
+            ensure!(
+                root_hash == account_root,
+                "Account storage root {} mismatch",
+                root_hash
+            );
+
+            // commit the in-memory trie to disk
+            let (keys, vals): (Vec<_>, Vec<_>) = mem_storage.storage.write().drain().unzip();
+            trie_storage.insert_batch(keys, vals)?;
+
+            account_count += 1;
+        }
+        // compute state_root_hash for parent block; also flushes nodes to disk.
+        let root_hash = account_storage.root_hash()?;
+        ensure!(
+            root_hash.0 == parent.state_root_hash().0,
+            "State root hash {} mismatch",
+            root_hash
+        );
+
+        (account_count, record_count)
+    };
+
+    ensure!(
+        meta.record_count == record_count,
+        "Record count {} mismatch",
+        record_count
+    );
+    ensure!(
+        meta.account_count == account_count,
+        "Account count {} mismatch",
+        account_count
+    );
+
+    Ok(())
+}
+
+pub fn save_ckpt(
+    path: &Path,
+    trie_storage: Arc<TrieStorage>,
+    block: Block,
+    transactions: Vec<SignedTransaction>,
+    parent: Block,
+    chain_id: u64,
+) -> Result<()> {
+    ensure!(
+        path.extension().unwrap_or_default() == "ckpt",
+        "Checkpoint {} is not .ckpt file",
+        path.display()
+    );
+
+    let zipfile = std::fs::File::create(path)?;
+    let options = zip::write::SimpleFileOptions::default()
+        .large_file(true)
+        .compression_method(zip::CompressionMethod::Zstd);
+
+    let mut zipwriter = zip::ZipWriter::new(zipfile);
+
+    // write block.json
+    zipwriter.start_file("block.bin", options)?;
+    bincode::serde::encode_into_std_write(&block, &mut zipwriter, BIN_CONFIG)?;
+
+    // write parent.json
+    zipwriter.start_file("parent.bin", options)?;
+    bincode::serde::encode_into_std_write(&parent, &mut zipwriter, BIN_CONFIG)?;
+
+    // write transactions.json
+    zipwriter.start_file("transactions.bin", options)?;
+    bincode::serde::encode_into_std_write(&transactions, &mut zipwriter, BIN_CONFIG)?;
+
+    zipwriter.start_file("state_trie.bin", options)?;
+    let state_trie_storage = trie_storage.clone();
+
+    let accounts =
+        EthTrie::new(state_trie_storage.clone()).at_root(parent.state_root_hash().into());
+    let account_storage = EthTrie::new(state_trie_storage.clone());
+
+    let mut account_count = 0;
+    let mut record_count = 0;
+    // iterate over accounts and save the accounts to the checkpoint file.
+    // do not save intermediate state trie values.
+    for (key, serialised_account) in accounts.iter() {
+        bincode::encode_into_std_write(&key, &mut zipwriter, BIN_CONFIG)?;
+        bincode::encode_into_std_write(&serialised_account, &mut zipwriter, BIN_CONFIG)?;
+
+        let account_root = bincode::serde::decode_from_slice::<Account, _>(
+            &serialised_account,
+            bincode::config::legacy(), // for backwards compatibility with existing data
+        )?
+        .0
+        .storage_root;
+
+        // iterate over account storage keys, and save them to the checkpoint file.
+        let account_trie = account_storage.at_root(account_root);
+        let count = account_trie.iter().count();
+        bincode::serde::encode_into_std_write(count, &mut zipwriter, BIN_CONFIG)?;
+        for (storage_key, storage_val) in account_trie.iter() {
+            bincode::encode_into_std_write(&storage_key, &mut zipwriter, BIN_CONFIG)?;
+            bincode::encode_into_std_write(&storage_val, &mut zipwriter, BIN_CONFIG)?;
+            record_count += 1;
+        }
+
+        account_count += 1;
+    }
+
+    let meta = Checkpoint {
+        account_count,
+        record_count,
+        chain_id,
+        block_hash: block.hash(),
+    };
+
+    zipwriter.start_file("metadata.json", options)?;
+    serde_json::to_writer(&mut zipwriter, &meta)?;
+
+    zipwriter.set_comment(CKPT_VERSION);
+    zipwriter.finish()?;
+    Ok(())
 }
