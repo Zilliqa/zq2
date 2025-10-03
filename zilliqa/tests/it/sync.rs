922a1dd78d1ba4f0a3a1a646938a336689ce765a
Shawn <shawn@zilliqa.com>
2025-10-03 17:04:35 +0800

    Add state migration integration test for checkpoint syncing

--- zilliqa/tests/it/sync.rs
+++ zilliqa/tests/it/sync.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 use ethers::{providers::Middleware, types::TransactionRequest};
 use tracing::info;
 use zilliqa::{cfg::Checkpoint, crypto::Hash};
@@ -29,51 +27,34 @@ async fn prune_interval(mut network: Network) {
 async fn base_height(mut network: Network) {
     // Populate network with transactions
     let wallet = network.genesis_wallet().await;
-    // Run until block 9 so that we can insert a tx in block 10 (note that this transaction may not *always* appear in the desired block, therefore we do not assert its presence later)
-    network.run_until_block(&wallet, 9.into(), 200).await;
-
-    let _hash = wallet
+    network.run_until_block_finalized(5, 200).await.unwrap();
+    wallet
         .send_transaction(TransactionRequest::pay(wallet.address(), 10), None)
         .await
         .unwrap()
         .tx_hash();
 
     // wait 10 blocks for checkpoint to happen - then 3 more to finalize that block
-    network.run_until_block(&wallet, 13.into(), 200).await;
+    network.run_until_block_finalized(13, 200).await.unwrap();
 
-    let checkpoint_files = network
-        .nodes
-        .iter()
-        .map(|node| {
-            node.dir
-                .as_ref()
-                .unwrap()
-                .path()
-                .join(network.shard_id.to_string())
-                .join("checkpoints")
-                .join("10")
-        })
-        .collect::<Vec<_>>();
-
-    let mut len_check = 0;
-    for path in &checkpoint_files {
-        let metadata = fs::metadata(path).unwrap();
-        assert!(metadata.is_file());
-        let file_len = metadata.len();
-        assert!(file_len != 0);
-        assert!(len_check == 0 || len_check == file_len); // len_check = 0 on first loop iteration
-        len_check = file_len;
-    }
+    let idx = network.random_index();
+    let checkpoint_path = network
+        .get_node_raw(idx)
+        .dir
+        .as_ref()
+        .unwrap()
+        .path()
+        .join("checkpoints")
+        .join("10");
 
     // Add a non-validator node, since passive-sync does not work otherwise
     let non_validator_idx = network.add_node();
 
     // Create new node and pass it one of those checkpoint files
-    let checkpoint_path = checkpoint_files[0].to_str().unwrap().to_owned();
     let checkpoint_hash = wallet.get_block(10).await.unwrap().unwrap().hash.unwrap();
     let new_node_idx = network.add_node_with_options(NewNodeOptions {
         checkpoint: Some(Checkpoint {
-            file: checkpoint_path,
+            file: checkpoint_path.to_str().unwrap().to_owned(),
             hash: Hash(checkpoint_hash.0),
         }),
         base_height: Some(3),
@@ -100,4 +81,68 @@ async fn base_height(mut network: Network) {
         .unwrap()
         .start();
     assert_eq!(base_height, 3);
+}
+
+#[zilliqa_macros::test(do_checkpoints)] // default blocks_per_epoch = 10, epochs_per_checkpoint = 1
+async fn state_migration(mut network: Network) {
+    // Populate network with transactions
+    let wallet = network.genesis_wallet().await;
+    network.run_until_block_finalized(5, 200).await.unwrap();
+    wallet
+        .send_transaction(TransactionRequest::pay(wallet.address(), 10), None)
+        .await
+        .unwrap()
+        .tx_hash();
+
+    network.run_until_block_finalized(15, 200).await.unwrap();
+    wallet
+        .send_transaction(TransactionRequest::pay(wallet.address(), 10), None)
+        .await
+        .unwrap()
+        .tx_hash();
+
+    network.run_until_block_finalized(25, 200).await.unwrap();
+
+    let idx = network.random_index();
+    let checkpoint_path = network
+        .get_node_raw(idx)
+        .dir
+        .as_ref()
+        .unwrap()
+        .path()
+        .join("checkpoints")
+        .join("20");
+
+    // Add a non-validator node, since passive-sync does not work otherwise
+    let non_validator_idx = network.add_node();
+
+    // Create new node and pass it one of those checkpoint files
+    let checkpoint_hash = wallet.get_block(20).await.unwrap().unwrap().hash.unwrap();
+    let new_node_idx = network.add_node_with_options(NewNodeOptions {
+        checkpoint: Some(Checkpoint {
+            file: checkpoint_path.to_str().unwrap().to_owned(),
+            hash: Hash(checkpoint_hash.0),
+        }),
+        base_height: Some(5),
+        ..Default::default()
+    });
+
+    // Confirm wallet and new_node_wallet have the same block and state
+    let new_node_wallet = network.wallet_of_node(new_node_idx).await;
+    let latest_block_number = new_node_wallet.get_block_number().await.unwrap();
+    assert_eq!(latest_block_number, 20.into());
+
+    // check the new node catches up and keeps up with block production
+    network.run_until_synced(non_validator_idx).await;
+    network.run_until_synced(new_node_idx).await;
+    network.run_until_block_finalized(30, 200).await.unwrap();
+
+    // check range of new wallet
+    let base_height = *network
+        .node_at(new_node_idx)
+        .db
+        .available_range()
+        .unwrap()
+        .start();
+    assert_eq!(base_height, 5);
 }
