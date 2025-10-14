b9e7950dea84adbf5d1f253d7ddb57989db09f1a
Shawn <shawn@zilliqa.com>
2025-10-14 15:09:25 +0800

    minor changes.

--- zilliqa/src/jsonrpc/mod.rs
+++ zilliqa/src/jsonrpc/mod.rs
@@ -1,4 +1,9 @@
-pub mod rpc_credit_list;
-pub mod rpc_credit_store;
-pub mod rpc_extension_layer;
-pub mod rpc_rate_limit;
+mod rpc_credit_store;
+mod rpc_extension_layer;
+mod rpc_price_list;
+mod rpc_rate_limit;
+
+pub use rpc_credit_store::*;
+pub use rpc_extension_layer::*;
+pub use rpc_price_list::*;
+pub use rpc_rate_limit::*;
