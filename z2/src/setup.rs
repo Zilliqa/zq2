aaabfa9e099e807f086a14becf91bd78fa4050f2
Shawn <shawn@zilliqa.com>
2025-10-15 12:42:15 +0800

    chore: fix tests, z2.

--- z2/src/setup.rs
+++ z2/src/setup.rs
@@ -520,7 +520,9 @@ impl Setup {
                 api_servers: vec![ApiServer {
                     port,
                     enabled_apis: api::all_enabled(),
+                    default_credit: None,
                 }],
+                credit_list: HashMap::new(),
                 allowed_timestamp_skew: allowed_timestamp_skew_default(),
                 data_dir: None,
                 state_cache_size: state_cache_size_default(),
