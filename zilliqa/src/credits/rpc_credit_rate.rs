5b0ada18a8638ad90ccbda77c07081f19ed7895b
Shawn <shawn@zilliqa.com>
2025-10-20 16:11:30 +0800

    rpc: allow user to specify [credit_rates.default = xxx].

--- zilliqa/src/credits/rpc_credit_rate.rs
+++ zilliqa/src/credits/rpc_credit_rate.rs
@@ -7,24 +7,33 @@ const DEFAULT_CREDIT: u64 = 500; // arbitrarily chosen
 #[derive(Debug, Clone)]
 pub struct RpcCreditRate {
     credits: DashMap<String, u64>,
+    default: u64,
 }
 
 impl RpcCreditRate {
     pub fn new(credit_list: HashMap<String, u64>) -> Self {
+        // get the default credit rate, if unspecified
+        //
+        // [credit_rates]
+        // default = 500
+        let default = credit_list
+            .get("default")
+            .map(|v| *v)
+            .unwrap_or(DEFAULT_CREDIT);
+
         let credits = DashMap::with_capacity(credit_list.len());
         for (key, value) in credit_list.into_iter() {
             credits.insert(key, value);
         }
-        // Zero fee for health check
+
+        // Zero fee for health checks
         credits.insert(String::from("health_check"), 0);
-        Self { credits }
+
+        Self { credits, default }
     }
 
     #[inline]
     pub fn get_credit(&self, method: &str) -> u64 {
-        self.credits
-            .get(method)
-            .map(|v| *v)
-            .unwrap_or(DEFAULT_CREDIT)
+        self.credits.get(method).map(|v| *v).unwrap_or(self.default)
     }
 }
