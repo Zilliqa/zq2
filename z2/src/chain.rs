804e50d2ba36eac29f5cc636ef47760afea74b60
shawn-zil <shawn@zilliqa.com>
2026-09-18 16:12:30 +0800

    update fork heights to 40408000-40409000 (#3771)

--- z2/src/chain.rs
+++ z2/src/chain.rs
@@ -331,9 +331,9 @@ impl Chain {
                 json!({ "at_height": 34369689, "allow_scilla_call_precompile_to_be_called_from_addresses": ["0x453b11386FBd54bC532892c0217BBc316fc7b918"]}),
                 json!({ "at_height": 39985504, "blocked_recipients_start_height": 39985504, "blocked_recipients_file": "blocked_recipients_001.bin" }),
                 json!({ "at_height": 39985504, "disable_zilliqa_txn_execution": true }),
-                json!({ "at_height": 40599771, "blocked_recipients_start_height_v2": 40599771, "blocked_recipients_file_v2": "blocked_recipients_002.bin" }),
-                json!({ "at_height": 40599771, "deploy_escrow_contract_v1": true }),
-                json!({ "at_height": 40599772, "zil_transfers_only_to_escrow": true }),
+                json!({ "at_height": 40408000, "blocked_recipients_start_height_v2": 40408000, "blocked_recipients_file_v2": "blocked_recipients_002.bin" }),
+                json!({ "at_height": 40408500, "deploy_escrow_contract_v1": true }),
+                json!({ "at_height": 40409000, "zil_transfers_only_to_escrow": true }),
             ]),
             Chain::Zq2Mainnet => Some(vec![
                 json!({ "at_height": 4770088, "executable_blocks": true }),
